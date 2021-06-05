71d6d10de504a6690f5aa4a4eb686b603fb77f47
carlotta carboni <carlotta.carboni2@studio.unibo.it>
2021-06-05 19:33:26 +0000

    Upload New File

---
+++ ConnectionListener.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.gui2.Label;
+import com.googlecode.lanterna.gui2.Panel;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.googlecode.lanterna.TextColor.ANSI.BLACK;
+
+public class ConnectionListener implements Runnable {
+    private HashMap<String, PrintWriter> connectedClients = new HashMap();
+    private Panel panel;
+    private TextColor coloreLabel;
+
+    public ConnectionListener(Panel panel,TextColor coloreLabel) {
+        this.panel=panel;
+        this.coloreLabel=coloreLabel;
+    }
+    @Override
+    public void run() {
+        try{
+            ServerSocket listener = null;
+            int port = 6789;
+            listener = new ServerSocket(port);
+            listener.setReuseAddress(true);
+            while(true){
+                Socket socket = listener.accept();
+                // creo un thread per ogni client così
+                // da essere gestiti singolarmente
+                ClientHandler clientSock = new ClientHandler(socket, "",panel,coloreLabel);
+                clientSock.clientHandler();
+                new Thread(clientSock).start();
+
+
+                Label lab_clientJoin=new Label("Connected clients: " + connectedClients.size() + "/8").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
+                panel.addComponent(lab_clientJoin);
+
+                System.out.println("Connected clients: " + connectedClients.size() + "/8");
+                broadcastServerMessage("[SERVER]: Connected clients: " + connectedClients.size() + "/8");
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void broadcastServerMessage(String message) {
+
+        for(Map.Entry<String, PrintWriter> e : connectedClients.entrySet()) {
+
+            e.getValue().println(message);
+            e.getValue().flush();
+        }
+    }
+}
