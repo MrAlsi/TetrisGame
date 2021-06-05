50648b378c6fbec1c0465b4d04a0f7c3104042ee
Alessandro Pasi <alessandro.pasi6@studio.unibo.it>
2021-06-05 15:03:16 +0000

    Miglioramento grafico, creazione classe Empty

--- MainSchermata.java
+++ MainSchermata.java
@@ -9,13 +9,17 @@ import com.googlecode.lanterna.gui2.Button;
 import com.googlecode.lanterna.gui2.GridLayout;
 import com.googlecode.lanterna.gui2.Label;
 import com.googlecode.lanterna.gui2.Panel;
+import com.googlecode.lanterna.gui2.Window;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
+import java.util.Arrays;
+
 import java.awt.*;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
@@ -27,6 +31,16 @@ import static com.googlecode.lanterna.TextColor.ANSI.BLUE;
 public class MainSchermata {
     public static void main(String[] args) throws IOException {
 
+        Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+
+        // width will store the width of the screen
+        int width = (int)size.getWidth();
+
+        // height will store the height of the screen
+        int height = (int)size.getHeight();
+
+        int leftMargin = width/19;
+        int topMargin = height/50;
 
         //codice per avere uno chermo
         Terminal terminal = new DefaultTerminalFactory().createTerminal();
@@ -36,15 +50,23 @@ public class MainSchermata {
 
         // Creo pannello
         final Panel panel = new Panel();
-        panel.setLayoutManager(new GridLayout(1));
+        panel.setLayoutManager(
+                new GridLayout(1)
+                        .setLeftMarginSize(leftMargin)
+                        .setRightMarginSize(0)
+                        .setTopMarginSize(topMargin));
         panel.setFillColorOverride(BLACK);
         panel.addComponent(new EmptySpace(new TerminalSize(0,0))); // Empty space underneath labels
-        //richiamo metodo per avviare la grafica
+        // richiamo metodo per avviare la grafica
         Schermata(panel);
 
 
         BasicWindow window = new BasicWindow();
+        // importante
+
+        window.setHints(Arrays.asList(Window.Hint.FULL_SCREEN));
         window.setComponent(panel);
+
 
 
         MultiWindowTextGUI gui = new MultiWindowTextGUI(screen, new DefaultWindowManager(), new EmptySpace(BLACK));
@@ -67,21 +89,23 @@ public class MainSchermata {
         panel.addComponent(tetris4);
     }
 
+    // separatore di dimensioni pari a size
+    public static void Empty(Panel panel, int size){
+        panel.addComponent(new EmptySpace(new TerminalSize(0,size)));
+    }
+
     //codice home
     public static void Schermata(final Panel panel){
         final TextColor coloreLabel=TextColor.ANSI.GREEN_BRIGHT;
-        Label testo=new Label("Benvenuto in:\n").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
-        panel.addComponent(testo);
+        // Label testo=new Label("Benvenuto in:\n").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
+        // panel.addComponent(testo);
 
         Tetris(panel);
 
-        Label lab=new Label("Vuoi accedere come Server o Client?\n").setBackgroundColor(BLACK).setForegroundColor(
-                coloreLabel);
-        panel.addComponent(lab);
-
+        Empty(panel, 1);
 
         //bottone client
-        new Button("Client", new Runnable() {
+        new Button("Find Game", new Runnable() {
             @Override
             public void run() {
                 panel.removeAllComponents();
@@ -92,7 +116,8 @@ public class MainSchermata {
                 //registrazione utente
                 Label user=new Label("\nName: ").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
                 TextBox textUser=new TextBox();// nome nuovo client
-                Label IP=new Label("\nIndirizzo IP Server: ").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
+                Empty(panel, 1);
+                Label IP=new Label("\nServer IP: ").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
                 TextBox textIP=new TextBox();//indirizzo IP del server a cui voglio collegarmi
 
                 panel.addComponent(user);
@@ -100,6 +125,7 @@ public class MainSchermata {
                 panel.addComponent(IP);
                 panel.addComponent(textIP);
 
+                Empty(panel, 1);
                 //bottone che mi riporta alla home
                 new Button("Indietro",new Runnable(){
                     @Override
@@ -113,12 +139,14 @@ public class MainSchermata {
             }
         }).addTo(panel);
 
+        Empty(panel, 1);
+
         //bottone per accedere come server
-        new Button("Server", new Runnable() {
+        new Button("Host Game", new Runnable() {
 
             public void run() {
 
-              //  System.out.println("sto eseguendo");
+                //  System.out.println("sto eseguendo");
                 panel.removeAllComponents();
                 panel.setFillColorOverride(BLACK);
                 //metodo per la scritta tetris
@@ -132,26 +160,27 @@ public class MainSchermata {
                 panel.addComponent(user);
                 panel.addComponent(textUser);
 
-                new Button("Avvia Server",new Runnable(){
-                        @Override
-                        public void run(){
-                            String name = textUser.getText();
-                            panel.removeAllComponents();
-                            panel.setFillColorOverride(BLACK);
-                            //dovrei chiamare il codice del server
+                Empty(panel, 1);
 
-                            Server server = new Server(name,panel,coloreLabel);
-                            server.StartServer(server);
+                new Button("Start Server",new Runnable(){
+                    @Override
+                    public void run(){
+                        String name = textUser.getText();
+                        panel.removeAllComponents();
+                        panel.setFillColorOverride(BLACK);
+                        //dovrei chiamare il codice del server
 
-                        }
-                    }).addTo(panel);
+                        Server server = new Server(name,panel,coloreLabel);
+                        server.StartServer(server);
 
-
+                    }
+                }).addTo(panel);
 
                 //System.out.println(" ciao");
                 //qui ci proviamo a mettere un bel semaforino
                 //server
 
+                Empty(panel, 1);
 
                 //bottone per tornare alla home
                 new Button("Indietro",new Runnable(){
@@ -165,23 +194,51 @@ public class MainSchermata {
 
             }
         }).addTo(panel);
-        Label felice=new Label("O vuoi farci felici e leggere chi siano i creatori del gioco? ;)\n").setBackgroundColor
-                (BLACK).setForegroundColor(coloreLabel);
-        panel.addComponent(felice);
-        //bottone per accedere al regolamento e ai crediti(sarebbe carino che se un client sta guardando il regolamento
-        // il server non possa avviare il gioco)
-        new Button("Crediti e regolamento", new Runnable(){
+
+        Empty(panel, 2);
+
+        Label separatore =new Label("~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~^~").setBackgroundColor(BLACK).setForegroundColor(TextColor.ANSI.WHITE);
+        panel.addComponent(separatore);
+
+        Empty(panel, 2);
+
+        new Button("Rules", new Runnable(){
             @Override
             public void run() {
                 panel.removeAllComponents();
                 panel.setFillColorOverride(BLACK);
-                Label regolamento=new Label("Ciao, queste sono le regole del nostro gioco:\n Si gioca da 2 fino a " +
-                        "un massimo di 8 giocatori,...").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
-                Label crediti=new Label("\n Questo gioco è stato realizzato da:\n Gabriel Riccardo Alsina,\n " +
-                        "Carlotta Carboni,\n Luca Palmieri,\n Alssandro Pasi.").setBackgroundColor(BLACK).setForegroundColor
-                        (coloreLabel);
+                Label regolamento = new Label("Regolamento:\n\nUna partita è formata da massimo 8 giocatori.\nVince chi resta più tempo in partita. ").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
                 panel.addComponent(regolamento);
+
+                Empty(panel, 1);
+                //bottone per tornare alla home
+                new Button("Indietro",new Runnable(){
+                    @Override
+                    public void run(){
+                        panel.removeAllComponents();
+                        panel.setFillColorOverride(BLACK);
+                        Schermata(panel);
+                    }
+                }).addTo(panel);
+            }
+
+        }).addTo(panel);
+
+        Empty(panel, 1);
+
+        //bottone per accedere al regolamento e ai crediti(sarebbe carino che se un client sta guardando il regolamento
+        // il server non possa avviare il gioco)
+        new Button("Credits", new Runnable(){
+            @Override
+            public void run() {
+                panel.removeAllComponents();
+                panel.setFillColorOverride(BLACK);
+
+                Label crediti=new Label("\nQuesto gioco è stato realizzato da:\n\n\nGabriel Riccardo Alsina,\n\nCarlotta Carboni,\n\nLuca Palmieri,\n\nAlssandro Pasi.").setBackgroundColor(BLACK).setForegroundColor
+                        (coloreLabel);
+
                 panel.addComponent(crediti);
+                Empty(panel, 1);
                 //bottone per tornare alla home
                 new Button("Indietro",new Runnable(){
                     @Override
