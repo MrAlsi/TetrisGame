f2941572ec7781c0d907079417b77374ab025b52
carlotta carboni <carlotta.carboni2@studio.unibo.it>
2021-06-05 19:31:46 +0000

    Update ClientHandler.java

--- ClientHandler.java
+++ ClientHandler.java
@@ -1,5 +1,12 @@
 package com.company;
 
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.gui2.Label;
+import com.googlecode.lanterna.gui2.Label;
+import com.googlecode.lanterna.gui2.Panel;
+
+
+
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -14,23 +21,28 @@ import java.util.*;
 import java.util.Map.Entry;
 import java.util.Arrays;
 
+import static com.googlecode.lanterna.TextColor.ANSI.BLACK;
 
-public  class ClientHandler implements Runnable{
+
+public  class ClientHandler implements Runnable {
     private Socket clientSocket;
-    public String username;
+    private String username;
     private BufferedReader fromClient;
-    HashMap<String ,PrintWriter> connectedClients;
+    private Panel panel;
+    private TextColor coloreLabel;
+    private HashMap<String, PrintWriter> connectedClients;
+
     // Constructor
-    public ClientHandler(Socket socket, String userName )
-    {
+    public ClientHandler(Socket socket, String userName,Panel panel,TextColor coloreLabel) {
         this.clientSocket = socket;
         this.username = userName;
+        this.panel=panel;
+        this.coloreLabel=coloreLabel;
     }
-    private void ClientHandler(){
+
+    public void clientHandler() {
         PrintWriter out = null;
-        try{
-
-
+        try {
             InputStream socketInput = clientSocket.getInputStream();
             InputStreamReader socketReader = new InputStreamReader(socketInput);
             fromClient = new BufferedReader(socketReader);
@@ -38,12 +50,19 @@ public  class ClientHandler implements Runnable{
             out = new PrintWriter(clientSocket.getOutputStream(), true);
 
             username = fromClient.readLine();
+
+            Label lab_newClient = new Label("New client connected: " + username + " ["
+                    + clientSocket.getInetAddress().getHostAddress()
+                    + "]").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
+            panel.addComponent(lab_newClient);
+
             System.out.println("New client connected: " + username + " ["
                     + clientSocket.getInetAddress().getHostAddress()
-                    + "]" );
+                    + "]");
+
             broadcastServerMessage("New client connected: " + username + " ["
                     + clientSocket.getInetAddress().getHostAddress()
-                    + "]" );
+                    + "]");
             connectedClients.put(username, out);
 
         } catch (Exception e) {
@@ -52,15 +71,19 @@ public  class ClientHandler implements Runnable{
 
     }
 
-    public void run()
-    {
+    @Override
+    public void run() {
         try {
             //LOGICA APPLICATIVA - RICEZIONE MESSAGGI
             String message = "";
-            while(message != null) { //Finché il client non chiude la connessione o non ricevi un messaggio "quit"...
+            while (message != null) { //Finché il client non chiude la connessione o non ricevi un messaggio "quit"...
                 message = fromClient.readLine(); //Leggi un messaggio inviato dal server (bloccante!)
                 if (message != null) {
                     if (message.toLowerCase().equals("/quit")) break;
+
+                    Label lab_clientMsg = new Label("[" + username + "]: " + message).setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
+                    panel.addComponent(lab_clientMsg);
+
                     System.out.println("[" + username + "]: " + message);
                     broadcastMessage(String.format("[%s]: %s", username, message), username);
 
@@ -75,6 +98,12 @@ public  class ClientHandler implements Runnable{
             if (username != null) {
                 if (true) System.out.println(username + " is leaving");
                 connectedClients.remove(username);
+
+                Label lab_clientLeft = new Label(username + " has left").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
+                panel.addComponent(lab_clientLeft);
+                Label lab_clientTot = new Label("Connected clients: " + connectedClients.size() + "/8").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
+                panel.addComponent(lab_clientTot);
+
                 System.out.println(username + " has left");
                 System.out.println("Connected clients: " + connectedClients.size() + "/8");
                 broadcastServerMessage(username + " has left");
@@ -82,6 +111,15 @@ public  class ClientHandler implements Runnable{
             }
         }
     }
+    public void broadcastServerMessage(String message) {
+
+        for(Entry<String, PrintWriter> e : connectedClients.entrySet()) {
+
+            e.getValue().println(message);
+            e.getValue().flush();
+        }
+    }
+
     public  void broadcastMessage(String message, String username) {
 
         for(Entry<String, PrintWriter> e : connectedClients.entrySet()) {
@@ -91,17 +129,6 @@ public  class ClientHandler implements Runnable{
                 e.getValue().println(message);
                 e.getValue().flush();
             }
-        }
-    }
-
-
-    // classe per la trasmissione di un messaggio inviato dal server agli altri client
-    public void broadcastServerMessage(String message) {
-
-        for(Entry<String, PrintWriter> e : connectedClients.entrySet()) {
-
-            e.getValue().println(message);
-            e.getValue().flush();
         }
     }
 }
