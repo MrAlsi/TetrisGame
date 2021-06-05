85af4cf2ac125267673311d8b64a3eea2677a443
carlotta carboni <carlotta.carboni2@studio.unibo.it>
2021-06-05 19:32:51 +0000

    Update Server.java classi separate invio messaggi no

--- Server.java
+++ Server.java
@@ -36,8 +36,8 @@ import static com.googlecode.lanterna.TextColor.ANSI.BLACK;
 
 public class Server  implements Runnable{
     private HashMap<String, PrintWriter> connectedClients = new HashMap();
-    private Server.ConnectionListener connectionListener = new Server.ConnectionListener();
-    private Thread listenerThread = new Thread(connectionListener);
+    private ConnectionListener connectionListener;
+    private Thread listenerThread ;
     protected Thread runningThread= null;
     private Boolean gameStart = false;
     public String name;
@@ -54,6 +54,8 @@ public class Server  implements Runnable{
         this.name = name;
         this.panel= panel;
         this.coloreLabel= coloreLabel;
+        connectionListener=new ConnectionListener(panel,coloreLabel);
+        listenerThread= new Thread(connectionListener);
 
     }
 
@@ -84,7 +86,7 @@ public class Server  implements Runnable{
             listenerThread.start();
             // creo il thread di comunicazione del server
             // e lo avvio
-            Server.ServerSender serverSender = new Server.ServerSender();
+            ServerSender serverSender = new ServerSender();
             Thread senderThread = new Thread(serverSender);
             senderThread.start();
 
@@ -118,118 +120,9 @@ public class Server  implements Runnable{
     }
 
     // classe per la gestione dei thread dei client
-    private class ClientHandler implements Runnable {
-        private Socket clientSocket;
-        public String username;
-        private BufferedReader fromClient;
 
-        // Constructor
-        public ClientHandler(Socket socket, String userName)
-        {
-            PrintWriter out = null;
-            try{
-                this.clientSocket = socket;
-                this.username = userName;
-                InputStream socketInput = clientSocket.getInputStream();
-                InputStreamReader socketReader = new InputStreamReader(socketInput);
-                fromClient = new BufferedReader(socketReader);
 
-                out = new PrintWriter(clientSocket.getOutputStream(), true);
 
-                username = fromClient.readLine();
-
-                Label lab_newClient=new Label( "New client connected: " + username + " ["
-                        + clientSocket.getInetAddress().getHostAddress()
-                        + "]").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
-                panel.addComponent(lab_newClient);
-
-                System.out.println("New client connected: " + username + " ["
-                        + clientSocket.getInetAddress().getHostAddress()
-                        + "]" );
-
-                broadcastServerMessage("New client connected: " + username + " ["
-                        + clientSocket.getInetAddress().getHostAddress()
-                        + "]" );
-                connectedClients.put(username, out);
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-        }
-        @Override
-        public void run()
-        {
-            try {
-                //LOGICA APPLICATIVA - RICEZIONE MESSAGGI
-                String message = "";
-                while(message != null) { //Finché il client non chiude la connessione o non ricevi un messaggio "quit"...
-                    message = fromClient.readLine(); //Leggi un messaggio inviato dal server (bloccante!)
-                    if (message != null) {
-                        if (message.toLowerCase().equals("/quit")) break;
-
-                        Label lab_clientMsg=new Label("[" + username + "]: " + message).setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
-                        panel.addComponent(lab_clientMsg);
-
-                        System.out.println("[" + username + "]: " + message);
-                        broadcastMessage(String.format("[%s]: %s", username, message), username);
-
-                    }
-                }
-
-                clientSocket.close(); //Interrompi la connessione
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                if (username != null) {
-                    if (true) System.out.println(username + " is leaving");
-                    connectedClients.remove(username);
-
-                    Label lab_clientLeft=new Label(username + " has left").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
-                    panel.addComponent(lab_clientLeft);
-                    Label lab_clientTot=new Label("Connected clients: " + connectedClients.size() + "/8").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
-                    panel.addComponent(lab_clientTot);
-
-                    System.out.println(username + " has left");
-                    System.out.println("Connected clients: " + connectedClients.size() + "/8");
-                    broadcastServerMessage(username + " has left");
-                    broadcastServerMessage("[SERVER]: Connected clients: " + connectedClients.size() + "/8");
-                }
-            }
-        }
-    }
-
-    public class ConnectionListener implements Runnable {
-
-        public ConnectionListener() {
-
-        }
-        @Override
-        public void run() {
-            try{
-                ServerSocket listener = null;
-                int port = 6789;
-                listener = new ServerSocket(port);
-                listener.setReuseAddress(true);
-                while(true){
-                    Socket socket = listener.accept();
-                    // creo un thread per ogni client così
-                    // da essere gestiti singolarmente
-                    Server.ClientHandler clientSock = new Server.ClientHandler(socket, "");
-                    new Thread(clientSock).start();
-
-                    Label lab_clientJoin=new Label("Connected clients: " + connectedClients.size() + "/8").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
-                    panel.addComponent(lab_clientJoin);
-
-                    System.out.println("Connected clients: " + connectedClients.size() + "/8");
-                    broadcastServerMessage("[SERVER]: Connected clients: " + connectedClients.size() + "/8");
-                }
-            } catch(Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
 
 
     // classe per la trasmissione di un messaggio inviato dal server agli altri client
@@ -241,7 +134,7 @@ public class Server  implements Runnable{
             e.getValue().flush();
         }
     }
-
+/*
     public  void broadcastMessage(String message, String username) {
 
         for(Entry<String, PrintWriter> e : connectedClients.entrySet()) {
@@ -272,33 +165,5 @@ public class Server  implements Runnable{
     }
     */
 
-    // classe per la gestione del thread del server
-    public  class ServerSender implements Runnable {
 
-        public ServerSender() {
-
-        }
-        @Override
-        public void run() {
-            TextBox textServer = new TextBox();
-            panel.addComponent(textServer);
-
-            Scanner serverInput = new Scanner(System.in);
-            String serverMessage = "";
-            String messageServer = "";
-            while(true) { //Finché non ricevi un comando "quit" dall'utente...
-                
-                messageServer = textServer.getText();
-                serverMessage = serverInput.nextLine(); //... leggi un messaggio da console (bloccante!)...
-                if(serverMessage.equals("/start")){
-                    listenerThread.interrupt();
-                    gameStart = true;
-                }
-                else if(serverMessage != null && !serverMessage.toLowerCase().equals("/start")){
-                    broadcastServerMessage("[SERVER]: " + messageServer);
-                    broadcastServerMessage("[SERVER]: " + serverMessage);
-                }
-            }
-        }
-    }
 }
