b33a31a4b3af74cc3abd6082a265ad4f300d1671
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-06-21 16:16:44 +0000

    Update Griglia.java

--- Gioco/Griglia.java
+++ Gioco/Griglia.java
@@ -1,22 +1,19 @@
-import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
-import com.googlecode.lanterna.screen.Screen;
 
 public class Griglia {
     Blocco[][] griglia;
     TextGraphics screen;
 
-    public Griglia(TextGraphics screen){
-        this.screen=screen;
-        griglia=new Blocco[12][24];
+    public Griglia(TextGraphics screen) {
+        this.screen = screen;
+        griglia = new Blocco[12][8];
     }
 
-    public void CreazioneCampo(){
-        for(int i=0; i<12; i++){
-            for(int e=0; e<24; e++){
-                griglia[i][e]=new BloccoVuoto(screen, i*5, e*3);
+    public void creaCampo() {
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 8; j++) {
+                griglia[i][j] = new BloccoVuoto(screen, i, j);
             }
         }
     }
-
 }
