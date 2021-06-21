272bff85c75fede9169fe24e06f529f4dfcccefc
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-06-21 17:00:54 +0000

    Update BloccoPieno.java

--- Gioco/BloccoPieno.java
+++ Gioco/BloccoPieno.java
@@ -2,30 +2,11 @@ import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 
 public class BloccoPieno extends Blocco{
-    int velocita=600;
+    int velocita=300;
 
-    public BloccoPieno(TextGraphics screen, int col, int rig) {
-        super(screen, col, rig, TextColor.ANSI.BLUE);
+    public BloccoPieno(TextGraphics screen, int colTerminale, int rigTerminale) {
+        super(screen, colTerminale, rigTerminale, TextColor.ANSI.BLUE);
         stato=1;
-    }
-
-    //Metodo per far scendere i vari blocchi
-    public void gravita(Griglia campo, int colonna, int riga){
-        try {
-            Thread.sleep(velocita);
-            if(campo.griglia[colonna][riga+1].getStato()==2){
-                campo.griglia[colonna][riga]=new BloccoStruttura(schermo, campo.griglia[colonna][riga].getPosizioneColonna(), campo.griglia[colonna][riga].getPosizioneRiga());
-            } else {
-                //Metto la casella dove sono stato come blocco vuoto
-                campo.griglia[colonna][riga] = new BloccoVuoto(schermo, campo.griglia[colonna][riga].getPosizioneColonna(), campo.griglia[colonna][riga].getPosizioneRiga());
-
-                //Riempo la successiva con un bloccoPieno
-                campo.griglia[colonna][riga + 1] = new BloccoPieno(schermo, campo.griglia[colonna][riga+1].getPosizioneColonna(), campo.griglia[colonna][riga+1].getPosizioneRiga());
-                System.out.println(riga);
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
     public void cambiaColore(){ }
@@ -36,31 +17,34 @@ public class BloccoPieno extends Blocco{
     }
 
 
-    //Metodo per spostare un blocco a destra
-    public int destra(Griglia campo, int colonna, int riga){
-        //Metto la casella dove sono stato come blocco vuoto
-        campo.griglia[colonna][riga] = new BloccoVuoto(schermo, campo.griglia[colonna][riga].getPosizioneColonna(), campo.griglia[colonna][riga].getPosizioneRiga());
+        //orizzontale: +1: sposta a destra  -1:sposta a sinistra
+        //Verticale: +1 scende      -1:sale
 
-        //Riempo la successiva con un bloccoPieno
-        campo.griglia[colonna+1][riga] = new BloccoPieno(schermo, campo.griglia[colonna+1][riga].getPosizioneColonna(), campo.griglia[colonna+1][riga].getPosizioneRiga());
-        System.out.println(colonna+" - " +riga);
-
-        //ritorno la nuova colonna
-        return colonna+1;
+    @Override
+    public void muovi(Griglia campo, int colGriglia, int rigGriglia, int orizzontale, int verticale){
+        try {
+            Thread.sleep(velocita);
+            //Metto la casella dove sono stato come blocco vuoto
+            campo.griglia[colGriglia][rigGriglia] = new BloccoVuoto(schermo, campo.griglia[colGriglia][rigGriglia].getColonna(), campo.griglia[colGriglia][rigGriglia].getRiga());
+            //Riempo la successiva con un bloccoPieno
+            campo.griglia[colGriglia+orizzontale][rigGriglia+verticale] = new BloccoPieno(schermo, campo.griglia[colGriglia+orizzontale][rigGriglia+verticale].getColonna(), campo.griglia[colGriglia+orizzontale][rigGriglia+verticale].getRiga());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    //Metodo per spostare un blocco a sinistra
-    public int sinistra(Griglia campo, int colonna, int riga){
-        //Metto la casella dove sono stato come blocco vuoto
-        campo.griglia[colonna][riga] = new BloccoVuoto(schermo, campo.griglia[colonna][riga].getPosizioneColonna(), campo.griglia[colonna][riga].getPosizioneRiga());
-
-        //Riempo la successiva con un bloccoPieno
-        campo.griglia[colonna-1][riga] = new BloccoPieno(schermo, campo.griglia[colonna-1][riga].getPosizioneColonna(), campo.griglia[colonna-1][riga].getPosizioneRiga());
-        System.out.println(colonna+" - " +riga);
-
-        //ritorno la nuova colonna
-        return colonna-1;
+    //Gestione delle collisioni
+    public boolean collisioneLaterale(Griglia campo, int x, int y, int spostamento){
+        if(x==0||x==11||campo.griglia[x+spostamento][y].getStato()>=2){
+            return false;
+        } else {
+            return true;
+        }
     }
 
+    @Override
+    public boolean collisioneSotto(Griglia campo){
+        return super.collisioneSotto(campo);
+    }
 
 }
