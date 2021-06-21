0ef7248ef78e6fabcc006af10cd4669c836b799f
Alessandro Pasi <alessandro.pasi6@studio.unibo.it>
2021-06-21 17:24:06 +0000

    Commenti aggiunti

--- ClientHandler.java
+++ ClientHandler.java
@@ -5,8 +5,6 @@ import com.googlecode.lanterna.gui2.Label;
 import com.googlecode.lanterna.gui2.Label;
 import com.googlecode.lanterna.gui2.Panel;
 
-
-
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -23,7 +21,8 @@ import java.util.Arrays;
 
 import static com.googlecode.lanterna.TextColor.ANSI.BLACK;
 
-
+// Per ogni client che si connetterà al server creo un Thread handlerThread che si occupa
+// di gestire singolarmente i client uno a uno
 public  class ClientHandler implements Runnable {
     private Socket clientSocket;
     private String username;
@@ -32,7 +31,6 @@ public  class ClientHandler implements Runnable {
     private TextColor coloreLabel;
     public HashMap<String, PrintWriter> connectedClients;
 
-    // Constructor
     public ClientHandler(Socket socket, String userName,Panel panel,TextColor coloreLabel, HashMap connectedClients) {
         this.clientSocket = socket;
         this.username = userName;
@@ -44,6 +42,7 @@ public  class ClientHandler implements Runnable {
     public void clientHandler() {
         PrintWriter out = null;
         try {
+
             InputStream socketInput = clientSocket.getInputStream();
             InputStreamReader socketReader = new InputStreamReader(socketInput);
             fromClient = new BufferedReader(socketReader);
@@ -52,6 +51,8 @@ public  class ClientHandler implements Runnable {
 
             username = fromClient.readLine();
 
+            // Appena il seguente client si connette al server lo comunico a tutti nella
+            // chat pre-partita
             Label lab_newClient = new Label("New client connected: " + username + " ["
                     + clientSocket.getInetAddress().getHostAddress()
                     + "]").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
@@ -75,13 +76,18 @@ public  class ClientHandler implements Runnable {
     @Override
     public void run() {
         try {
-            //LOGICA APPLICATIVA - RICEZIONE MESSAGGI
+        
             String message = "";
-            while (message != null) { //Finché il client non chiude la connessione o non ricevi un messaggio "quit"...
-                message = fromClient.readLine(); //Leggi un messaggio inviato dal server (bloccante!)
+            while (message != null) {
+
+                // Quando un client invia un messaggio viene ricevuto dal server qui 
+                message = fromClient.readLine(); 
                 if (message != null) {
+
+                    // Se il messaggio ricevuto dal client è /quit il server esce dal ciclo e finisce nel "finally"
                     if (message.toLowerCase().equals("/quit")) break;
 
+                    // Il server si occupa poi di trasmettere il messaggio agli altri client 
                     Label lab_clientMsg = new Label("[" + username + "]: " + message).setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
                     panel.addComponent(lab_clientMsg);
 
@@ -97,6 +103,9 @@ public  class ClientHandler implements Runnable {
             e.printStackTrace();
         } finally {
             if (username != null) {
+
+                // Quando un giocatore invia il comando /quit si disconnette dal server
+                // Viene quindi mandato un messaggio di aggiornamento a tutti i client
                 if (true) System.out.println(username + " is leaving");
                 connectedClients.remove(username);
 
@@ -112,6 +121,8 @@ public  class ClientHandler implements Runnable {
             }
         }
     }
+
+
     public void broadcastServerMessage(String message) {
 
         for(Entry<String, PrintWriter> e : connectedClients.entrySet()) {
@@ -121,6 +132,7 @@ public  class ClientHandler implements Runnable {
         }
     }
 
+    // Questo metodo serve a trasmettere il messaggio di un client a tutti gli altri tranne a se stesso
     public  void broadcastMessage(String message, String username) {
 
         for(Entry<String, PrintWriter> e : connectedClients.entrySet()) {
