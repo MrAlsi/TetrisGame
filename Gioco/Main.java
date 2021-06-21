9e10adb9dfd21c511a731d0e1ec3c604dca10664
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-06-21 16:15:13 +0000

    Update Main.java

--- Gioco/Main.java
+++ Gioco/Main.java
@@ -1,84 +1,117 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.gui2.BasicWindow;
-import com.googlecode.lanterna.gui2.DefaultWindowManager;
-import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
-import javax.swing.*;
-import javax.swing.plaf.basic.BasicSplitPaneUI;
-import java.awt.*;
-import java.awt.event.KeyListener;
 import java.io.IOException;
+import java.util.Random;
 
 public class Main {
 
-    public static void main(String[] args){
-        try{
+    public static void main(String[] args) throws InterruptedException {
+        try {
             //Creazione del terminale
-            Terminal terminal=new DefaultTerminalFactory().createTerminal();
-            //Panel panel = new Panel();
-           // panel.setPreferredSize(new TerminalSize(40,30));
-            //Quello che devo risolvere è:
-            //Inizializzare il terminale con una "size" e tenerla fissa
-            // terminal.addResizeListener();
-            //new TerminalSize(1000,100);
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
 
             BasicWindow window = new BasicWindow();
-            window.setFixedSize((new TerminalSize(100,50)));
+            window.setFixedSize((new TerminalSize(100, 50)));
 
             Screen screen = new TerminalScreen(terminal);
             screen.startScreen();
 
-/*
-            MultiWindowTextGUI gui=new MultiWindowTextGUI(screen, new DefaultWindowManager());
-            gui.addWindowAndWait(panel)
-*/
-            TextGraphics schermo=screen.newTextGraphics();
+            TextGraphics schermo = screen.newTextGraphics();
 
             //Creazione del campo da gioco
 
             Griglia campo = new Griglia(schermo);
-            campo.CreazioneCampo();
+            campo.creaCampo();
             screen.refresh();
 
             //Gioco
 
             //Cordinate nella griglia
-            int x=4;    //Colonne  |||
-            int y=0;    //Righe    ===
+            int x = 4;    //Colonne  |||
+            int y = 0;    //Righe    ===
 
             //Ho creato un blocco chiamato Mago (il mago del blocco) per testare il codice
             //Ovviamente andrà tolto
-            campo.griglia[x][y]=new BloccoPieno(schermo, campo.griglia[x][y].getPosizioneColonna(), campo.griglia[x][y].getPosizioneRiga());
-            BloccoPieno mago = (BloccoPieno) campo.griglia[x][y];
-            screen.refresh();
+            //BloccoPieno mago = (BloccoPieno) campo.griglia[x][y];
+            //screen.refresh();
 
-            while(true){
-                KeyStroke keyStroke = screen.pollInput();
+            KeyStroke keyStroke = screen.pollInput();
+            //Sostituisco le decisioni dell'utente con un numero random
+            Random rand = new Random();
+            int scelta;
 
-               // x=mago.destra(campo, x, y);
-                for(y=0; y<7; y++){
-
-                    //Spostamento
-                    Thread.sleep(100);
-
-                    //Gravità
-                    mago.gravita(campo, x, y);
-
+            do {
+                PezzoLungo p1 = (PezzoLungo) creaPezzo(schermo, campo);
+                screen.refresh();
+                for (int i = 0; i < 7; i++) {
+                    p1.scendi(campo, 0, 1);
+                    System.out.println("Scende sto cazzo di blocco");
                     screen.refresh();
                 }
-                if(x==200){
-                    break;
+            } while (true);
+
+/*
+            PROVA GIOCO
+            while(true){
+                x=4;
+                y=0;
+                BloccoPieno mago = new BloccoPieno(schermo, campo.griglia[x][y].getPosizioneColonna(), campo.griglia[x][y].getPosizioneRiga());
+                System.out.println("Nuovo ciclo");
+                while(y<7){
+                    scelta = rand.nextInt(6);
+                    System.out.println(scelta);
+                    switch(scelta){
+                        case 0: //Vai a destra
+                            if(mago.collisioneLaterale(campo, x,y,1)==true){
+                                mago.muovi(campo, x, y, 1, 0);
+                                screen.refresh();
+                                x++;
+                                System.out.println("Destra");
+                                break;
+                            } else {
+                                break;
+                            }
+
+                        case 1: //Vai a sinistra
+                            if(mago.collisioneLaterale(campo, x,y,1)==true){
+                                mago.muovi(campo, x, y, -1, 0);
+                                screen.refresh();
+                                x--;
+                                System.out.println("Sinistra");
+                                break;
+                            } else {
+                                break;
+                            }
+
+                        default: //Vai giù
+                            if(mago.collisioneSotto(campo, x, y)==true) {
+                                mago.muovi(campo, x, y, 0, 1);
+                                screen.refresh();
+                                y++;
+                                System.out.println("Giù");
+                                break;
+                            } else {
+                                campo.griglia[x][y]=new BloccoStruttura(schermo, campo.griglia[x][y].getPosizioneColonna(), campo.griglia[x][y].getPosizioneRiga());
+                                screen.refresh();
+                                System.out.println("Y. " + y);
+                                y++;
+                                break;
+                            }
+                    }
+                    Thread.sleep(600);
                 }
             }
-            screen.refresh();
-            //screen.stopScreen();
-        } catch (IOException | InterruptedException e) {
+
+
+        //screen.stopScreen();*/
+        } catch (IOException e) {
             System.out.println("Problema con il terminale");
             e.printStackTrace();
         }
@@ -86,12 +119,24 @@ public class Main {
 
     }
 
-    public void stampaGriglia(Griglia campo){
-        for(int i=0; i<12; i++){
-            for(int e=0; e<24; e++){
-                System.out.print(campo.griglia[i][e].getStato()+"  ");
+    public void stampaGriglia(Griglia campo) {
+        for (int i = 0; i < 12; i++) {
+            for (int e = 0; e < 24; e++) {
+                System.out.print(campo.griglia[i][e].getStato() + "  ");
             }
             System.out.println("");
         }
+    }
+
+    public static Pezzo creaPezzo(TextGraphics schermo, Griglia campo) {
+        PezzoLungo p1 = new PezzoLungo(
+                campo,
+                new BloccoPieno(schermo, 3, 0),
+                new BloccoPieno(schermo, 4, 0),
+                new BloccoPieno(schermo, 5, 0),
+                new BloccoPieno(schermo, 6, 0)
+        );
+
+        return p1;
     }
 }
