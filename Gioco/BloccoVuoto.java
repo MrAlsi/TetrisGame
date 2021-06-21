37ab7d9e81844c2e88eb1e220bf3965cb5c12df4
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-06-21 16:59:05 +0000

    Update BloccoVuoto.java

--- Gioco/BloccoVuoto.java
+++ Gioco/BloccoVuoto.java
@@ -5,6 +5,6 @@ import com.googlecode.lanterna.screen.Screen;
 public class BloccoVuoto extends Blocco{
     public BloccoVuoto(TextGraphics screen, int col, int rig) {
         super(screen, col, rig, TextColor.ANSI.DEFAULT);
-
+        stato=0;
     }
 }
