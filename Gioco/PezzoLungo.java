e92cee3b5f51ea4b098819737191cdab3f504e41
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-06-21 17:04:14 +0000

    Upload New File

---
+++ Gioco/PezzoLungo.java
@@ -0,0 +1,5 @@
+public class PezzoLungo extends Pezzo{
+    public PezzoLungo(Griglia campo, BloccoPieno b0, BloccoPieno b1, BloccoPieno b2, BloccoPieno b3) {
+        super(campo,b0, b1, b2, b3);
+    }
+}
