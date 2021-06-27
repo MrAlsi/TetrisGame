fa69424520d6e385c0a61634bf150973f5cf704f
carlotta carboni <carlotta.carboni2@studio.unibo.it>
2021-06-27 14:51:50 +0000

    Se sono meno di 2 giocatori il gioco non parte ServerSender.java

--- ServerSender.java
+++ ServerSender.java
@@ -2,7 +2,6 @@ package com.company;
 
 import com.googlecode.lanterna.gui2.Label;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,60 +12,71 @@ import com.googlecode.lanterna.gui2.Button;
 import com.googlecode.lanterna.gui2.Label;
 import com.googlecode.lanterna.gui2.Panel;
 import com.googlecode.lanterna.gui2.TextBox;
-
-import static com.company.MainSchermata.Schermata;
 import static com.googlecode.lanterna.TextColor.ANSI.BLACK;
 
-import static com.googlecode.lanterna.TextColor.ANSI.BLACK;
 
+import static com.googlecode.lanterna.TextColor.ANSI.BLACK;
 
 public class ServerSender implements Runnable{
 
     public HashMap<String ,PrintWriter> connectedClients;
     private Panel panel;
     private TextColor coloreLabel;
+    private String name;
 
-
-    public ServerSender(Panel panel, TextColor coloreLabel, HashMap connectedClients) {
-
+    public ServerSender(Panel panel, TextColor coloreLabel, HashMap connectedClients, String name) {
+        this.name=name;
         this.panel=panel;
         this.coloreLabel=coloreLabel;
         this.connectedClients=connectedClients;
 
     }
 
-    // Aggiungo la textbox al pannello pre-partita del server
     public void run() {
 
-        TextBox messaggio = new TextBox();
+        TextBox messaggio=new TextBox();
         panel.addComponent(messaggio);
         inviaMessaggio(messaggio);
 
     }
 
-    // Aggiungo il pulsante "Send" al pannello pre-partita del server
     public void inviaMessaggio(final TextBox messaggio){
-        new Button("Send",new Runnable(){
+        new Button("Invia",new Runnable(){
             @Override
             public void run(){
+                //Finché non ricevi un comando "quit" dall'utente...
+                //userMessage = userInput.nextLine(); //... leggi un messaggio da console (bloccante!)...
+                //toOther.println(userMessage); //... e invialo al server
 
-                // Posso inviare il messaggio solo nel caso non sia "vuoto"
+
                 if(!messaggio.getText().equals("")) {
                     String messaggioString = messaggio.getText();
 
-                    // Se il server invia il messaggio "/start" il gioco parte
+                    // Se il server invia il messaggio "/start" il gioco ccerca di partire
                     if(messaggioString.equals("/start")){
-                        panel.removeAllComponents();
-                        panel.setVisible(false);
-                        broadcastServerMessage(messaggioString);
+                        //controllo il numero dei giocatori
+                        if (connectedClients.size()+1<2) {
+                            // se con il server (quindi +1) siamo meno di 2 non può iniziare il gioco
+                            Label lab_serverMsg = new Label("[SERVER]: Non siete abbastanza giocatori." ).setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
+                            panel.addComponent(lab_serverMsg);
+
+                        }else {
+                            //svuoto il pannello e avverto i client
+                            panel.removeAllComponents();
+                            panel.setVisible(false);
+                            broadcastServerMessage(messaggioString);
+
+
+                        }
+
 
                     } else{
-
                         // In tutti gli altri casi trasmetto il messaggio del server a tutti i client connessi
-                        Label lab_serverMsg = new Label("[SERVER]: " + messaggioString).setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
+                        Label lab_serverMsg = new Label("["+name+"]: " + messaggioString).setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
                         panel.addComponent(lab_serverMsg);
 
-                        broadcastServerMessage("[SERVER]: " + messaggioString);
+                        broadcastServerMessage("["+name+"]: " + messaggioString);
+
                     }
 
                     // Una volta inviato il messaggio pulisco la textbox
@@ -74,28 +84,8 @@ public class ServerSender implements Runnable{
                 }
             }
         }).addTo(panel);
-
-        // Aggiungo il pulsante "indietro" alla schermata pre-partita del server
-        new Button("Close",new Runnable(){
-            @Override
-            public void run(){
-
-                // Una volta premuto spengo il server e torno alla home
-                // Da sistemare
-                try {
-
-                    System.exit(0);
-
-                } catch (Exception e) {
-
-                    e.printStackTrace();
-
-                }
-            }
-        }).addTo(panel);
     }
 
-    // Metodo per inoltrare il messaggio del server a tutti i client
     public void broadcastServerMessage(String message) {
 
         for(Map.Entry<String, PrintWriter> e : connectedClients.entrySet()) {
