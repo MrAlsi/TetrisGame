7b84d81b0069c6b728de371a229fb04d95cc47f2
Alessandro Pasi <alessandro.pasi6@studio.unibo.it>
2021-06-30 14:36:38 +0000

    Replace Griglia.java

--- Gioco/Griglia.java
+++ Gioco/Griglia.java
@@ -1,3 +1,5 @@
+package com.company.Gioco;
+
 import com.googlecode.lanterna.graphics.TextGraphics;
 
 public class Griglia {
@@ -6,12 +8,12 @@ public class Griglia {
 
     public Griglia(TextGraphics screen) {
         this.screen = screen;
-        griglia = new Blocco[12][8];
+        griglia = new Blocco[12][24];
     }
 
     public void creaCampo() {
         for (int i = 0; i < 12; i++) {
-            for (int j = 0; j < 8; j++) {
+            for (int j = 0; j < 24; j++) {
                 griglia[i][j] = new BloccoVuoto(screen, i, j);
             }
         }
