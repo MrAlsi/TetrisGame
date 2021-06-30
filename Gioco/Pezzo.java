62fd5c2c0eb57f1b1cbf3745d1794fda4d71523b
Alessandro Pasi <alessandro.pasi6@studio.unibo.it>
2021-06-30 14:38:05 +0000

    Replace Pezzo.java

--- Gioco/Pezzo.java
+++ Gioco/Pezzo.java
@@ -1,4 +1,7 @@
+package com.company.Gioco;
+
 public class Pezzo {
+
     public Blocco[] pezzo;
     Griglia campo;
 
@@ -15,7 +18,7 @@ public class Pezzo {
 
     }
 
-    public boolean scendi(Griglia campo, int orizzontale, int verticale) {
+    public boolean scendi(Griglia campo) {
         //System.out.println(pezzo[0].getPosizioneGrigliaColonna()+"  --  "+ pezzo[0].getPosizioneGrigliaRiga());
         if (collisioneSotto()) {
             for (int i = 0; i < 4; i++) {
@@ -34,11 +37,41 @@ public class Pezzo {
         }
     }
 
+    public void muovi(Griglia campo, int orizzontale){
+        if(!collisioneLaterale(orizzontale)){ //deve ritornare false per entrare
+            if(orizzontale==1){
+                for (int i = 3; i >= 0; i--) {
+                    pezzo[i].muovi(campo, pezzo[i].getColonna(), pezzo[i].getRiga(), orizzontale, 0);
+                    pezzo[i].mosso(orizzontale);
+                }
+            } else {
+                for (int i = 0; i < 4; i++) {
+                    pezzo[i].muovi(campo, pezzo[i].getColonna(), pezzo[i].getRiga(), orizzontale, 0);
+                    pezzo[i].mosso(orizzontale);
+                }
+            }
+        }
+    }
+
+    public boolean collisioneLaterale(int spostamento){ //Spostamento 1: Destra  -1: Sinistra
+        boolean collide = pezzo[0].collisioneLaterale(campo, spostamento) ||
+                pezzo[1].collisioneLaterale(campo, spostamento) ||
+                pezzo[2].collisioneLaterale(campo, spostamento) ||
+                pezzo[3].collisioneLaterale(campo, spostamento);
+        return collide;
+    }
+
+    // collisione con la struttura laterale, esempio: se a destra ho un blocco struttura non posso andarci
+    // ma devo semplicemente continuare a scendere
+    public boolean collisioneStruttura(){
+        return false;
+    }
+
     public boolean collisioneSotto() {
 
-        boolean collide = pezzo[0].collisioneSotto(campo) &&
-                pezzo[1].collisioneSotto(campo) &&
-                pezzo[2].collisioneSotto(campo) &&
+        boolean collide = pezzo[0].collisioneSotto(campo) ||
+                pezzo[1].collisioneSotto(campo) ||
+                pezzo[2].collisioneSotto(campo) ||
                 pezzo[3].collisioneSotto(campo);
 
         return collide;
