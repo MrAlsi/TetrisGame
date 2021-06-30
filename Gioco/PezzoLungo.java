d18e5a9ed340388905b8c62fc7b4cb201afe6415
Alessandro Pasi <alessandro.pasi6@studio.unibo.it>
2021-06-30 14:38:17 +0000

    Replace PezzoLungo.java

--- Gioco/PezzoLungo.java
+++ Gioco/PezzoLungo.java
@@ -1,3 +1,5 @@
+package com.company.Gioco;
+
 public class PezzoLungo extends Pezzo{
     public PezzoLungo(Griglia campo, BloccoPieno b0, BloccoPieno b1, BloccoPieno b2, BloccoPieno b3) {
         super(campo,b0, b1, b2, b3);
