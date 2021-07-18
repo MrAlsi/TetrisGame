a7fcfa2e4914051fb70982797c51a6781a62f9f0
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-07-18 15:59:05 +0000

    Aggiornato metodo rimuovi

--- Gioco/BloccoPieno.java
+++ Gioco/BloccoPieno.java
@@ -27,15 +27,17 @@ public class BloccoPieno extends Blocco{
     @Override
     public void muovi(Griglia campo, int colGriglia, int rigGriglia, int orizzontale, int verticale){
 
-            //Riempo la successiva con un bloccoPieno
-            campo.griglia[colGriglia+orizzontale][rigGriglia+verticale] = new BloccoPieno(schermo, campo.griglia[colGriglia+orizzontale][rigGriglia+verticale].getColonna(), campo.griglia[colGriglia+orizzontale][rigGriglia+verticale].getRiga());
+        //Riempo la successiva con un bloccoPieno
+        campo.griglia[colGriglia+orizzontale][rigGriglia+verticale] =
+                new BloccoPieno(schermo,
+                        campo.griglia[colGriglia+orizzontale][rigGriglia+verticale].getColonna(),
+                        campo.griglia[colGriglia+orizzontale][rigGriglia+verticale].getRiga());
 
     }
 
-    public void rimuovi(Griglia campo, int colGriglia, int rigGriglia, int orizzontale, int verticale){
-
+    public void rimuovi(Griglia campo, int colGriglia, int rigGriglia){
         //Metto la casella dove sono stato come blocco vuoto
-        campo.griglia[colGriglia][rigGriglia] = new BloccoVuoto(schermo, campo.griglia[colGriglia][rigGriglia].getColonna(), campo.griglia[colGriglia][rigGriglia].getRiga());
+        campo.griglia[colGriglia][rigGriglia] = new BloccoVuoto(schermo, colGriglia, rigGriglia);
     }
 
     //Gestione delle collisioni
