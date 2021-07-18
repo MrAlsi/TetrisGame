3c071341e740c112d3b6afc443e8724e81c8e99f
carlotta carboni <carlotta.carboni2@studio.unibo.it>
2021-07-18 16:14:52 +0000

    c'è da verificare che non crei il bottone anche per il giocatore stesso

--- Gioco/SelezioneSpazzatura.java
+++ Gioco/SelezioneSpazzatura.java
@@ -1,4 +1,4 @@
-package com.company.Gioco;
+ package com.company;
 
 import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
@@ -19,15 +19,18 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import static com.googlecode.lanterna.TextColor.ANSI.BLACK;
 
 public class SelezioneSpazzatura implements Runnable {
     private HashMap<String, PrintWriter> connectedClients;
+    private String nome;
 
-    public SelezioneSpazzatura(HashMap connectedClient) {
+    public SelezioneSpazzatura(HashMap connectedClient, String nome) {
         this.connectedClients = connectedClients;
+        this.nome = nome;
     }
 
     @Override
@@ -36,9 +39,7 @@ public class SelezioneSpazzatura implements Runnable {
 
         try {
             //@todo
-            //passare connectedclients
-            //passare numero giocatori
-            //devo sapere chi sono(almeno il mio nome)
+            //è da verificare che non crei il bottone anche per il mio nome
 
 
             final TextColor coloreLabel = TextColor.ANSI.GREEN_BRIGHT;
@@ -70,35 +71,38 @@ public class SelezioneSpazzatura implements Runnable {
             pannello.addComponent(presentazione);
             // salvo la dimensione di connectedClients
             //int dim = 4;
-            int dim=connectedClients.size();
+            int dim = connectedClients.size();
             //creo bottoni in base al numero dei giocatori
-            for (int i = 0; i < dim; i++) {
-                String nome = "";
-                new Button(nome + i, new Runnable() {
-                    @Override
-                    public void run() {
-                        //metodo per lanciare linee spazzatura
-                        //devo prendere i valori del bottone cliccato e richiamare il metodo per mandare linee spazzatura
-                        //Righespazzatura();
-                        pannello.removeAllComponents();
+            for (String i : connectedClients.keySet()) {
+                String name = String.valueOf(connectedClients.get(i));
+                //controllo che il nome sia diverso dal mio per non avere il bottone con il mio nome
+                if (!name.equals(nome)) {
+                    //assegno ad ogni bottone il nome di un giocatore
+                    new Button(name, new Runnable() {
+                        @Override
+                        public void run() {
+                            //metodo per lanciare linee spazzatura
+                            //devo prendere i valori del bottone cliccato e richiamare il metodo per mandare linee spazzatura
+                            //Righespazzatura();
+                            pannello.removeAllComponents();
+                        }
+                    }).addTo(pannello);
 
-
-                    }
-                }).addTo(pannello);
-
+                }
             }
 
-            BasicWindow window = new BasicWindow();
-            window.setHints(Arrays.asList(Window.Hint.FULL_SCREEN));
+                BasicWindow window = new BasicWindow();
+                window.setHints(Arrays.asList(Window.Hint.FULL_SCREEN));
 
 
-            window.setComponent(pannello);
-            MultiWindowTextGUI gui = new MultiWindowTextGUI(screen, new DefaultWindowManager(), new EmptySpace(BLACK));
-            gui.addWindowAndWait(window);
-        } catch (Exception e) {
-            e.printStackTrace();
+                window.setComponent(pannello);
+                MultiWindowTextGUI gui = new MultiWindowTextGUI(screen, new DefaultWindowManager(), new EmptySpace(BLACK));
+                gui.addWindowAndWait(window);
+        } catch(Exception e){
+                e.printStackTrace();
         }
 
 
     }
 }
+
