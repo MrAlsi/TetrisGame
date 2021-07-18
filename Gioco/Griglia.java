ea15fbfd15b9d29c91a28b5ebe215bd7e87ef8ec
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-07-18 16:04:55 +0000

    Aggiunto metodi controlloRighe, eliminaRiga, cadutaStruttura e sconfitta

--- Gioco/Griglia.java
+++ Gioco/Griglia.java
@@ -1,9 +1,11 @@
 package com.company.Gioco;
 
-import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 
-import static com.googlecode.lanterna.TextColor.ANSI.WHITE;
+
+//NELLA GRIGLIA
+//Colonne  ||||
+//Righe    ====
 
 public class Griglia {
     Blocco[][] griglia;
@@ -15,28 +17,60 @@ public class Griglia {
     }
 
     public void creaCampo() {
-        Boolean chess = false;
+        Boolean chess = false;  //Si può togliere?
         for (int i = 0; i < 12; i++) {
             for (int j = 0; j < 24; j++) {
                 griglia[i][j] = new BloccoVuoto(screen, i, j);
             }
         }
     }
-    public void eliminaRiga(){
-        Boolean riga = false;
-        for (int i = 0; i < 24; i++) {
-            riga = false;
-            for (int j = 0; j < 12; j++) {
-                if(griglia[j][i].getStato() == 2){
-                    riga = true;
+
+    public void controlloRighe(){
+        int combo = 0;              //Combo per le riga spazzatura
+        boolean elimina = false;        //Se non entra mai nel if viene segnata come riga da eliminare
+
+        for(int i = 0; i < 24; i++){
+            for(int j = 0; j<12; j++){
+                if(griglia[j][i].stato!=2){     //Se trova una sola cassella vuota (!=2) quella riga non si deve eliminare
+                    elimina=false;
+                    break;
                 }
-                riga = false;
+                elimina=true;
             }
-            if(riga){
-                for (int y = 0; y < 12; y++) {
-                    griglia[y][i] = new BloccoVuoto(screen, y, i);
+            if(elimina) {
+                eliminaRiga(i);                 //Elimina le righe che gli passano
+                cadutaStruttura(i);             //Fa cadere la struttura di righe che ha eliminato
+            }
+        }
+    }
+
+    public void eliminaRiga(int riga){
+        for(int i=0; i<12; i++){
+            griglia[i][riga] = new BloccoVuoto(screen, griglia[i][riga].colonnaGriglia, griglia[i][riga].rigaGriglia);
+        }
+    }
+
+    public void cadutaStruttura(int riga){
+        for(int i = riga - 1; i >= 0; i--){
+            for(int j = 0; j < 12; j++){
+                if(griglia[j][i].stato==2) {
+                    griglia[j][i] = new BloccoVuoto(screen,
+                                                    griglia[j][i].colonnaGriglia,
+                                                    griglia[j][i].rigaGriglia);
+
+                    griglia[j][i + 1] = new BloccoStruttura(screen,
+                                                            griglia[j][i + 1].colonnaGriglia,
+                                                            griglia[j][i + 1].rigaGriglia);
                 }
             }
         }
+    }
+
+    //Un po' una maialata, però cosi si perde
+    public boolean sconfitta(){
+        if(griglia[4][0].stato>1||griglia[5][0].stato>1||griglia[6][0].stato>1||griglia[3][0].stato>1){
+            return true;
+        }
+        return false;
     }
 }
