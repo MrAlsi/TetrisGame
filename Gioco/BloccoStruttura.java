721985ee7f5b767a7cd9b27499efbb8f1f152ff1
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-07-19 14:26:46 +0000

    Update BloccoStruttura.java #Colore

--- Gioco/BloccoStruttura.java
+++ Gioco/BloccoStruttura.java
@@ -7,7 +7,7 @@ import java.lang.reflect.GenericDeclaration;
 
 public class BloccoStruttura extends Blocco{
     public BloccoStruttura(TextGraphics schermo, int col, int rig) {
-        super(schermo, col, rig, TextColor.ANSI.GREEN);
+        super(schermo, col, rig, TextColor.ANSI.WHITE_BRIGHT);
         stato = 2;
     }
 
