5c42062514367645076a3b7fdf474d64b96444e6
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-07-19 14:31:51 +0000

    Update PezzoQuadrato.java #Colore

--- Gioco/PezzoQuadrato.java
+++ Gioco/PezzoQuadrato.java
@@ -3,6 +3,9 @@ package com.company.Gioco;
 import com.googlecode.lanterna.graphics.TextGraphics;
 
 public class PezzoQuadrato extends Pezzo{
+
+    TextColor colore = TextColor.ANSI.CYAN_BRIGHT;
+
     public PezzoQuadrato(TextGraphics schermo, Griglia campo) {
         super(campo,new BloccoPieno(schermo, 3, 1),
                 new BloccoPieno(schermo, 4, 1),
