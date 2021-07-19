b0d73432030fa97ba5f6bbb32870b99341da8e9d
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-07-19 14:30:34 +0000

    Update PezzoL.java #Colore

--- Gioco/PezzoL.java
+++ Gioco/PezzoL.java
@@ -1,13 +1,17 @@
 package com.company.Gioco;
 
+import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 
 public class PezzoL extends Pezzo {
+
+    public TextColor colore = TextColor.ANSI.YELLOW_BRIGHT;
+
     public PezzoL(TextGraphics schermo, Griglia campo) {
-        super(campo, new BloccoPieno(schermo, 3, 1),
-                new BloccoPieno(schermo, 4, 1),
-                new BloccoPieno(schermo, 5, 1),
-                new BloccoPieno(schermo, 5, 0));
+        super(campo, new BloccoPieno(schermo, 3, 1, TextColor.ANSI.YELLOW_BRIGHT),
+                new BloccoPieno(schermo, 4, 1, TextColor.ANSI.YELLOW_BRIGHT),
+                new BloccoPieno(schermo, 5, 1, TextColor.ANSI.YELLOW_BRIGHT),
+                new BloccoPieno(schermo, 5, 0, TextColor.ANSI.YELLOW_BRIGHT));
         tipoPezzo = 2;
         rotazione = 1;
     }
@@ -21,19 +25,19 @@ public class PezzoL extends Pezzo {
             pezzo[1].rimuovi(campo, pezzo[1].getColonna(), pezzo[1].getRiga());
             pezzo[0].rimuovi(campo, pezzo[0].getColonna(), pezzo[0].getRiga());
 
-            pezzo[3].muovi(campo, pezzo[3].getColonna(), pezzo[3].getRiga(), 2, 0);
+            pezzo[3].muovi(campo, pezzo[3].getColonna(), pezzo[3].getRiga(), 2, 0, colore);
             pezzo[3].colonnaGriglia = pezzo[3].getColonna() + 2;
             pezzo[3].rigaGriglia = pezzo[3].getRiga();
 
-            pezzo[2].muovi(campo, pezzo[2].getColonna(), pezzo[2].getRiga(), 1, 1);
+            pezzo[2].muovi(campo, pezzo[2].getColonna(), pezzo[2].getRiga(), 1, 1, colore);
             pezzo[2].colonnaGriglia = pezzo[2].getColonna() + 1;
             pezzo[2].rigaGriglia = pezzo[2].getRiga() + 1;
 
-            pezzo[1].muovi(campo, pezzo[1].getColonna(), pezzo[1].getRiga(), 0, 0);
+            pezzo[1].muovi(campo, pezzo[1].getColonna(), pezzo[1].getRiga(), 0, 0, colore);
             pezzo[1].colonnaGriglia = pezzo[1].getColonna();
             pezzo[1].rigaGriglia = pezzo[1].getRiga();
 
-            pezzo[0].muovi(campo, pezzo[0].getColonna(), pezzo[0].getRiga(), -1, -1);
+            pezzo[0].muovi(campo, pezzo[0].getColonna(), pezzo[0].getRiga(), -1, -1, colore);
             pezzo[0].colonnaGriglia = pezzo[0].getColonna() - 1;
             pezzo[0].rigaGriglia = pezzo[0].getRiga() - 1;
 
@@ -47,19 +51,19 @@ public class PezzoL extends Pezzo {
             pezzo[1].rimuovi(campo, pezzo[1].getColonna(), pezzo[1].getRiga());
             pezzo[0].rimuovi(campo, pezzo[0].getColonna(), pezzo[0].getRiga());
 
-            pezzo[3].muovi(campo, pezzo[3].getColonna(), pezzo[3].getRiga(), 0, 2);
+            pezzo[3].muovi(campo, pezzo[3].getColonna(), pezzo[3].getRiga(), 0, 2, colore);
             pezzo[3].colonnaGriglia = pezzo[3].getColonna();
             pezzo[3].rigaGriglia = pezzo[3].getRiga() + 2;
 
-            pezzo[2].muovi(campo, pezzo[2].getColonna(), pezzo[2].getRiga(), -1, 1);
+            pezzo[2].muovi(campo, pezzo[2].getColonna(), pezzo[2].getRiga(), -1, 1, colore);
             pezzo[2].colonnaGriglia = pezzo[2].getColonna() - 1;
             pezzo[2].rigaGriglia = pezzo[2].getRiga() + 1;
 
-            pezzo[1].muovi(campo, pezzo[1].getColonna(), pezzo[1].getRiga(), 0, 0);
+            pezzo[1].muovi(campo, pezzo[1].getColonna(), pezzo[1].getRiga(), 0, 0, colore);
             pezzo[1].colonnaGriglia = pezzo[1].getColonna();
             pezzo[1].rigaGriglia = pezzo[1].getRiga();
 
-            pezzo[0].muovi(campo, pezzo[0].getColonna(), pezzo[0].getRiga(), 1, -1);
+            pezzo[0].muovi(campo, pezzo[0].getColonna(), pezzo[0].getRiga(), 1, -1, colore);
             pezzo[0].colonnaGriglia = pezzo[0].getColonna() + 1;
             pezzo[0].rigaGriglia = pezzo[0].getRiga() - 1;
 
@@ -73,19 +77,19 @@ public class PezzoL extends Pezzo {
             pezzo[1].rimuovi(campo, pezzo[1].getColonna(), pezzo[1].getRiga());
             pezzo[0].rimuovi(campo, pezzo[0].getColonna(), pezzo[0].getRiga());
 
-            pezzo[3].muovi(campo, pezzo[3].getColonna(), pezzo[3].getRiga(), -2, 0);
+            pezzo[3].muovi(campo, pezzo[3].getColonna(), pezzo[3].getRiga(), -2, 0, colore);
             pezzo[3].colonnaGriglia = pezzo[3].getColonna() - 2;
             pezzo[3].rigaGriglia = pezzo[3].getRiga();
 
-            pezzo[2].muovi(campo, pezzo[2].getColonna(), pezzo[2].getRiga(), -1, -1);
+            pezzo[2].muovi(campo, pezzo[2].getColonna(), pezzo[2].getRiga(), -1, -1, colore);
             pezzo[2].colonnaGriglia = pezzo[2].getColonna() - 1;
             pezzo[2].rigaGriglia = pezzo[2].getRiga() - 1;
 
-            pezzo[1].muovi(campo, pezzo[1].getColonna(), pezzo[1].getRiga(), 0, 0);
+            pezzo[1].muovi(campo, pezzo[1].getColonna(), pezzo[1].getRiga(), 0, 0, colore);
             pezzo[1].colonnaGriglia = pezzo[1].getColonna();
             pezzo[1].rigaGriglia = pezzo[1].getRiga();
 
-            pezzo[0].muovi(campo, pezzo[0].getColonna(), pezzo[0].getRiga(), 1, 1);
+            pezzo[0].muovi(campo, pezzo[0].getColonna(), pezzo[0].getRiga(), 1, 1, colore);
             pezzo[0].colonnaGriglia = pezzo[0].getColonna() + 1;
             pezzo[0].rigaGriglia = pezzo[0].getRiga() + 1;
 
@@ -99,19 +103,19 @@ public class PezzoL extends Pezzo {
             pezzo[1].rimuovi(campo, pezzo[1].getColonna(), pezzo[1].getRiga());
             pezzo[0].rimuovi(campo, pezzo[0].getColonna(), pezzo[0].getRiga());
 
-            pezzo[3].muovi(campo, pezzo[3].getColonna(), pezzo[3].getRiga(), 0, -2);
+            pezzo[3].muovi(campo, pezzo[3].getColonna(), pezzo[3].getRiga(), 0, -2, colore);
             pezzo[3].colonnaGriglia = pezzo[3].getColonna();
             pezzo[3].rigaGriglia = pezzo[3].getRiga() - 2;
 
-            pezzo[2].muovi(campo, pezzo[2].getColonna(), pezzo[2].getRiga(), 1, -1);
+            pezzo[2].muovi(campo, pezzo[2].getColonna(), pezzo[2].getRiga(), 1, -1, colore);
             pezzo[2].colonnaGriglia = pezzo[2].getColonna() + 1;
             pezzo[2].rigaGriglia = pezzo[2].getRiga() - 1;
 
-            pezzo[1].muovi(campo, pezzo[1].getColonna(), pezzo[1].getRiga(), 0, 0);
+            pezzo[1].muovi(campo, pezzo[1].getColonna(), pezzo[1].getRiga(), 0, 0, colore);
             pezzo[1].colonnaGriglia = pezzo[1].getColonna();
             pezzo[1].rigaGriglia = pezzo[1].getRiga();
 
-            pezzo[0].muovi(campo, pezzo[0].getColonna(), pezzo[0].getRiga(), -1, 1);
+            pezzo[0].muovi(campo, pezzo[0].getColonna(), pezzo[0].getRiga(), -1, 1, colore);
             pezzo[0].colonnaGriglia = pezzo[0].getColonna() - 1;
             pezzo[0].rigaGriglia = pezzo[0].getRiga() + 1;
 
