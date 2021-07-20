f7b57481dad3425000b74437e520006338c54acc
carlotta carboni <carlotta.carboni2@studio.unibo.it>
2021-07-20 09:37:53 +0000

    Tolto il main e aggiunto costruttore e run

--- Gioco/GameOver.java
+++ Gioco/GameOver.java
@@ -13,48 +13,61 @@ import java.util.Arrays;
 
 import static com.googlecode.lanterna.TextColor.ANSI.BLACK;
 //classe per la schermata di inizializzazione del gioco
-public class GameOver {
-    public static void main(String[] args) throws IOException {
+public class GameOver implements Runnable{
+    public GameOver(){
 
-        final int COLS = 50;
-        final int ROWS = 8;
-        DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
-        defaultTerminalFactory.setInitialTerminalSize(new TerminalSize(COLS,ROWS));
-        Terminal terminal = defaultTerminalFactory.createTerminal();
-        BasicWindow window = new BasicWindow();
-        window.setFixedSize((new TerminalSize(45, 5)));
-        //Terminal terminal = new DefaultTerminalFactory().createTerminal();
-        final Screen screen = new TerminalScreen(terminal);
-        screen.startScreen();
+    }
+    @Override
+    public void run() {
 
 
-        // Creo pannello
-        final Panel panel = new Panel();
+        try {
+            final int COLS = 50;
+            final int ROWS = 8;
+            DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
+            defaultTerminalFactory.setInitialTerminalSize(new
+
+                    TerminalSize(COLS, ROWS));
+            Terminal terminal = defaultTerminalFactory.createTerminal();
+            BasicWindow window = new BasicWindow();
+            window.setFixedSize((new
+
+                    TerminalSize(45, 5)));
+            //Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            final Screen screen = new TerminalScreen(terminal);
+            screen.startScreen();
+
+
+            // Creo pannello
+            final Panel panel = new Panel();
      /*  panel.setLayoutManager(
                 new GridLayout(1)
                         .setLeftMarginSize(leftMargin)
                         .setRightMarginSize(0)
                         .setTopMarginSize(topMargin)); */
-        panel.setFillColorOverride(BLACK);
-      //  panel.addComponent(new EmptySpace(new TerminalSize(0,0))); // Empty space underneath labels
-        // richiamo metodo per avviare la grafica
-        Schermata(panel);
+            panel.setFillColorOverride(BLACK);
+
+            //  panel.addComponent(new EmptySpace(new TerminalSize(0,0))); // Empty space underneath labels
+            // richiamo metodo per avviare la grafica
+            Schermata(panel);
 
 
-        BasicWindow windoww = new BasicWindow();
-        // importante
-        windoww.setHints(Arrays.asList(Window.Hint.FULL_SCREEN));
-        //window.setFixedSize((new TerminalSize(10,20)));
+            BasicWindow windoww = new BasicWindow();
+            // importante
+            windoww.setHints(Arrays.asList(Window.Hint.FULL_SCREEN));
+            //window.setFixedSize((new TerminalSize(10,20)));
 
-        window.setComponent(panel);
+            window.setComponent(panel);
 
 
-
-        MultiWindowTextGUI gui = new MultiWindowTextGUI(screen, new DefaultWindowManager(), new EmptySpace(BLACK));
-        gui.addWindowAndWait(window);
+            MultiWindowTextGUI gui = new MultiWindowTextGUI(screen, new DefaultWindowManager(), new EmptySpace(BLACK));
+            gui.addWindowAndWait(window);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     //codice scritta colorata gameover
-    public static void GameOver(Panel panel){
+    public static void GameOver(Panel panel) {
        /* Label gameover1=new Label("\n   ___   ___  ___  ___  ____      ___   __ __  ____ ____ \n").setBackgroundColor(BLACK).setForegroundColor
                 (TextColor.ANSI.RED);
         Label gameover2=new Label("  // \\\\ // \\\\ ||\\\\//|| ||        // \\\\  || || ||    || \\\\\n" ).setBackgroundColor(BLACK).setForegroundColor
@@ -63,35 +76,31 @@ public class GameOver {
                 (TextColor.ANSI.GREEN_BRIGHT);
         Label gameover4=new Label("  \\\\_|| || || ||    || ||___     \\\\_//   \\V/  ||___ || \\\\\n").setBackgroundColor(BLACK).setForegroundColor
                 (TextColor.ANSI.BLUE_BRIGHT);*/
-        Label gameover1=new Label("\n  ____ ____ _  _ ____    ____ _  _ ____ ____  \n").setBackgroundColor(BLACK).setForegroundColor
+        Label gameover1 = new Label("\n  ____ ____ _  _ ____    ____ _  _ ____ ____  \n").setBackgroundColor(BLACK).setForegroundColor
                 (TextColor.ANSI.RED);
-        Label gameover2=new Label("  | __ |__| |\\/| |___    |  | |  | |___ |__/ \n" ).setBackgroundColor(BLACK).setForegroundColor
+        Label gameover2 = new Label("  | __ |__| |\\/| |___    |  | |  | |___ |__/ \n").setBackgroundColor(BLACK).setForegroundColor
                 (TextColor.ANSI.RED_BRIGHT);
-        Label gameover3=new Label("  |__] |  | |  | |___    |__|  \\/  |___ |  \\ \n").setBackgroundColor(BLACK).setForegroundColor
+        Label gameover3 = new Label("  |__] |  | |  | |___    |__|  \\/  |___ |  \\ \n").setBackgroundColor(BLACK).setForegroundColor
                 (TextColor.ANSI.GREEN_BRIGHT);
-
 
 
         panel.addComponent(gameover1);
         panel.addComponent(gameover2);
         panel.addComponent(gameover3);
-      //  panel.addComponent(gameover4);
+        //  panel.addComponent(gameover4);
     }
 
     // separatore di dimensioni pari a size
-    public static void Empty(Panel panel, int size){
-        panel.addComponent(new EmptySpace(new TerminalSize(0,size)));
+    public static void Empty(Panel panel, int size) {
+        panel.addComponent(new EmptySpace(new TerminalSize(0, size)));
     }
 
     //codice home
-    public static void Schermata(final Panel panel){
-        final TextColor coloreLabel=TextColor.ANSI.GREEN_BRIGHT;
+    public static void Schermata(final Panel panel) {
+        final TextColor coloreLabel = TextColor.ANSI.GREEN_BRIGHT;
 
         GameOver(panel);
 
 
-
-
     }
-
 }
