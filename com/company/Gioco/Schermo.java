d0df7b2c24c2d6d3980f8002e3f536357124ee4c
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-08-30 12:42:34 +0000

    Update Schermo.java

--- com/company/Gioco/Schermo.java
+++ com/company/Gioco/Schermo.java
@@ -1,14 +1,14 @@
 package com.company.Gioco;
 
+import com.company.Gioco.Mini.*;
+import com.company.Gioco.Pezzi.*;
 import com.company.client.Client;
+import com.googlecode.lanterna.Symbols;
+import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
-import com.googlecode.lanterna.gui2.*;
-import com.googlecode.lanterna.gui2.Button;
-import com.googlecode.lanterna.gui2.GridLayout;
 import com.googlecode.lanterna.gui2.Panel;
-import com.googlecode.lanterna.gui2.Window;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
@@ -16,16 +16,11 @@ import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
-import javax.management.StringValueExp;
-import javax.swing.plaf.basic.BasicOptionPaneUI;
 import java.awt.*;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
-import java.lang.String;
 
 import static com.googlecode.lanterna.TextColor.ANSI.BLACK;
 
@@ -33,22 +28,21 @@ public class Schermo implements Runnable{
 
     public int delay = 1000 / 60;
 
-    Mini_Griglia[] miniCampo = new Mini_Griglia[3];
-
     private Griglia campo;
     private TextGraphics schermo;
     private Pezzo pezzoScelto;
+    private Mini_Pezzo Pezzettino1;
     Random sceltaPezzo = new Random();
     private String azione;
     public static boolean gameOver = false;
-    public static String datas;
+    public static String datas = "e";
     private CadutaBlocco brickDropTimer;
     private Boolean barra = false;
     public static String usernameDestinatario = "palma";
     public static int aggiungiSpazzatura = 0;
     private List<String> connectedClients;
 
-    public PrintWriter pw;
+    public PrintWriter pw = null;
     private String username;
     private String IP;
     private int PORT;
@@ -56,6 +50,10 @@ public class Schermo implements Runnable{
     private TextColor coloreLabel;
     private final int brickDropDelay = 1000;
     private Screen screen;
+
+    private int miaGriglia[][] = new int[12][24];
+
+    public Mini_Griglia[] miniCampo = new Mini_Griglia[3];
 
     public Schermo(PrintWriter toServer, String name, String IP, int PORT, Panel panel, TextColor coloreLabel, List<String> connectedClients) throws IOException {
         this.IP = IP;
@@ -144,12 +142,55 @@ public class Schermo implements Runnable{
         screen.refresh();
     }
 
+    public Schermo() throws IOException {
+        Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+
+        System.out.println(size);
+        System.out.println(size.getWidth());
+        System.out.println(size.getHeight());
+
+        // width will store the width of the screen
+        int width = (int)size.getWidth();
+
+        // height will store the height of the screen
+        int height = (int)size.getHeight();
+
+        int leftMargin = width/19;
+        int topMargin = height/50;
+
+        //codice per avere uno chermo
+        final int COLS = width/8;
+        final int ROWS = height/16;
+        DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
+        defaultTerminalFactory.setInitialTerminalSize(new TerminalSize(COLS,ROWS));
+        Terminal terminal = defaultTerminalFactory.createTerminalEmulator();
+
+        screen = new TerminalScreen(terminal);
+        screen.startScreen();
+
+        //Creazione del campo da gioco
+        schermo = screen.newTextGraphics();
+
+        campo = new Griglia(schermo);
+        campo.creaCampo();
+
+        miniCampo[0] = new Mini_Griglia(schermo, 0,"a");
+        miniCampo[0].creaCampo();
+
+        miniCampo[1] = new Mini_Griglia(schermo, 1,"b");
+        miniCampo[1].creaCampo();
+
+        miniCampo[2] = new Mini_Griglia(schermo, 2,"c");
+        miniCampo[2].creaCampo();
+
+        screen.refresh();
+    }
 
     public synchronized void run(){
         try {
             //Creazione terminale con dimensioni già fisse
 
-            //com.company.Gioco
+            //Gioco
 
             // start brick move treads
             brickDropTimer = new CadutaBlocco();
@@ -160,30 +201,43 @@ public class Schermo implements Runnable{
             keyInput.start();
 
             pezzoScelto = prossimoPezzo(schermo);
+
+            //Pezzettino1 = new Mini_PezzoLungo(schermo, miniCampo[1]);
+            //Mini_PezzoLungo pezzettino = new Mini_PezzoLungo(schermo, miniCampo[1]);
             // run game loop
             while(!gameOver) {
 
+                //evidenzia(0);
+                traduciGrigliaToString(campo);
+                traduciStringToGriglia(miaGriglia, miniCampo[1]);
                 screen.refresh();
 
                 if(Client.winner == true){
                     Client.winner = false;
                     System.out.println("Partita finita");
-                    YouWin vittoria = new YouWin(username, IP, PORT, panel, coloreLabel,connectedClients);
+                    YouWin vittoria = new YouWin(username, IP, PORT, panel, coloreLabel, connectedClients);
                     Thread vittoriaThread = new Thread(vittoria);
                     vittoriaThread.start();
+                    traduciGrigliaToString(campo);
+                    traduciStringToGriglia(miaGriglia, miniCampo[1]);
                     break;
                 }
 
                 List<KeyStroke> keyStrokes = keyInput.getKeyStrokes();
 
                 for(KeyStroke key : keyStrokes) {
+                    traduciGrigliaToString(campo);
+                    traduciStringToGriglia(miaGriglia, miniCampo[1]);
                     screen.refresh();
                     processKeyInput(key);
+
                 }
 
                 if((pezzoScelto.collisioneSotto() && brickDropTimer.getDropBrick()) || barra){
                     barra = false;                               //Avviene una collisione: TRUE
                     pezzoScelto.setStruttura();                  //Il pezzo diventa parte della struttura
+                    traduciGrigliaToString(campo);
+                    traduciStringToGriglia(miaGriglia, miniCampo[1]);               //Aggiorna il proprio stato la griglia
                     screen.refresh();                            //Refresh dello schermo
                     int combo = campo.controlloRighe();          //Controllo se ci sono righe piene
                     if(combo > 1){                               //Combo serve per vedere se si sono liberate più righe
@@ -197,26 +251,34 @@ public class Schermo implements Runnable{
                         System.out.println("Partita finita");
                         String msg_sconfitta = username + "-lost";
                         invia(msg_sconfitta, pw);
-                        GameOver sconfitta = new GameOver(username, IP, PORT, panel, coloreLabel,connectedClients);
+                        GameOver sconfitta = new GameOver(username, IP, PORT, panel, coloreLabel, connectedClients);
                         Thread sconfittaThread = new Thread(sconfitta);
                         sconfittaThread.start();
                         gameOver = true;
                         break;
                     }
                     pezzoScelto = prossimoPezzo(schermo);        //Nuovo pezzo inizia a scendere
-                }
+                    traduciGrigliaToString(campo);
+                    traduciStringToGriglia(miaGriglia, miniCampo[1]);                }
 
                 if(aggiungiSpazzatura != 0){
                     campo.aggiungiSpazzatura(aggiungiSpazzatura);
+                    traduciGrigliaToString(campo);
+                    traduciStringToGriglia(miaGriglia, miniCampo[1]);
                     aggiungiSpazzatura = 0;
                 }
 
                 if(brickDropTimer.getDropBrick()) {
                     screen.refresh();
                     pezzoScelto.scendi(campo);
+                    traduciGrigliaToString(campo);
+                    traduciStringToGriglia(miaGriglia, miniCampo[1]);
+                    //Pezzettino1.scendi(miniCampo[1], 1);
                     datas = username + "/" + pezzoScelto.tipoPezzo + pezzoScelto.getCoord();
                     invia(datas, pw);
                 }
+                traduciGrigliaToString(campo);
+                traduciStringToGriglia(miaGriglia, miniCampo[1]);
             }
             screen.close();
             Thread.currentThread().interrupt();
@@ -228,12 +290,14 @@ public class Schermo implements Runnable{
     }
 
     public void stampaGriglia(Griglia campo) {
+        System.out.println("-------------------------------------");
         for (int i = 0; i < 12; i++) {
             for (int e = 0; e < 24; e++) {
                 System.out.print(campo.griglia[i][e].getStato() + "  ");
             }
             System.out.println("");
         }
+        System.out.println("-------------------------------------");
     }
 
     //Restituisce il prossimo pezzo che cadrà
@@ -273,6 +337,9 @@ public class Schermo implements Runnable{
         Character c1 = ' ';
         Character c2 = 'z';
         Character c3 = 'x';
+        Character uno = '1'; //Evidenziare campo 1 (in realtà lo 0)
+        Character due = '2'; //Evidenziare campo 2 (in realtà lo 1)
+        Character tre = '3'; //Evidenziare campo 3 (in realtà lo 2)
         datas = "";
 
         //down totale
@@ -330,20 +397,59 @@ public class Schermo implements Runnable{
             invia(datas, pw);
             screen.refresh();
         }
+
+        if(uno.equals(key.getCharacter())){
+            evidenzia(0);
+        }
+        if(due.equals(key.getCharacter())){
+            evidenzia(1);
+        }
+        if(tre.equals(key.getCharacter())){
+            evidenzia(2);
+        }
     }
 
-    public void invia(String s, PrintWriter pw){
+    /*public void invia(String s, PrintWriter pw){
         pw.println(s);
         pw.flush();
-    }
+    }*/
 
     public void righeSpazzatura(int combo){
         switch(combo){
             //Prova per vedere se funziona, quando mettiamo le righe spazzatura useremo questo metodo
-            case 2 : System.out.println("1 Riga spazzatura");
-            case 3 : System.out.println("2 Riga spazzatura");
-            case 4 : System.out.println("4 Riga spazzatura");
-            break;
+            case 2 -> System.out.println("1 Riga spazzatura");
+            case 3 -> System.out.println("2 Riga spazzatura");
+            case 4 -> System.out.println("4 Riga spazzatura");
+        }
+    }
+
+    public void evidenzia(int campo){
+        campo=campo*40+60-1;
+        schermo.drawRectangle(new TerminalPosition(campo, 2), new TerminalSize(26, 26),
+                Symbols.BLOCK_SOLID).setForegroundColor(TextColor.ANSI.YELLOW_BRIGHT);
+    }
+
+    public void traduciGrigliaToString(Griglia griglia){
+        for(int i=0; i<griglia.griglia.length; i++){
+            for(int e=0; e<griglia.griglia[i].length; e++){
+                switch (griglia.griglia[i][e].getStato()) {
+                    case 0 -> miaGriglia[i][e]=0;
+                    case 1 -> miaGriglia[i][e]=1;
+                    case 2 -> miaGriglia[i][e]=2;
+                }
+            }
+        }
+    }
+
+    public void traduciStringToGriglia(int[][] griglia, Mini_Griglia miniGriglia){
+        for(int i=0; i<griglia.length; i++){
+            for(int e=0; e<griglia[i].length; e++){
+                switch (griglia[i][e]) {
+                    case 0 -> miniGriglia.griglia[i][e]=new Mini_BloccoVuoto(schermo, i, e, 1);
+                    case 1 -> miniGriglia.griglia[i][e]=new Mini_BloccoPieno(schermo, i, e, TextColor.ANSI.WHITE_BRIGHT);
+                    case 2 -> miniGriglia.griglia[i][e]=new Mini_BloccoStruttura(schermo, i, e);
+                }
+            }
         }
     }
 }
