a440226b60ad07830fff87580d99909ace3ce46f
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-09-01 16:18:02 +0000

    Add new file

---
+++ com/company/Gioco/InviaStato.java
@@ -0,0 +1,51 @@
+package com.Gioco;
+
+import java.io.PrintWriter;
+
+public class InviaStato implements Runnable{
+    int[][] miaGriglia = new int[12][24];
+    PrintWriter pw;
+    String username;
+
+    public InviaStato(String username, PrintWriter pw){
+        this.pw=pw;
+        this.username=username;
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public void run(Griglia griglia) {
+        traduciGrigliaToInt(griglia);
+    }
+
+    public synchronized void traduciGrigliaToInt(Griglia griglia){
+        for(int i=0; i<griglia.griglia.length; i++){
+            for(int e=0; e<griglia.griglia[i].length; e++){
+                switch (griglia.griglia[i][e].getStato()) {
+                    case 0 :{miaGriglia[i][e]=0;
+                        break;}
+                    case 1 :{miaGriglia[i][e]=1;
+                        break;}
+                    case 2 :{miaGriglia[i][e]=2;
+                        break;}
+                }
+            }
+        }
+        traduciInttoString(miaGriglia);
+    }
+
+    public synchronized void traduciInttoString(int[][] mat){
+        String stringa= username + ":";
+        for(int i=0; i<12; i++){
+            for(int e=0; e<24; e++){
+                stringa=stringa+(mat[i][e]);
+            }
+        }
+        //System.out.println(stringa);
+        Schermo.invia(stringa, pw);
+
+    }
+}
