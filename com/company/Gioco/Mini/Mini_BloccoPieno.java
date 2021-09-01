7d988e8234eab979540431026875c03a2f19cd54
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-09-01 13:46:56 +0000

    Update Mini_BloccoPieno.java

--- com/company/Gioco/Mini/Mini_BloccoPieno.java
+++ com/company/Gioco/Mini/Mini_BloccoPieno.java
@@ -4,8 +4,8 @@ import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 
 public class Mini_BloccoPieno extends Mini_Blocco {
-    public Mini_BloccoPieno(TextGraphics screen, int colGriglia, int rigGriglia, TextColor colore){
-        super(screen, colGriglia, rigGriglia, colore,1);
+    public Mini_BloccoPieno(TextGraphics screen, int colGriglia, int rigGriglia, TextColor colore, int scostamento){
+        super(screen, colGriglia, rigGriglia, colore,scostamento);
         stato=1;
     }
 }
