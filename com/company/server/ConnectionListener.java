a912a9767d7622591d6aca5767f6a37b71765430
Alessandro Pasi <alessandro.pasi6@studio.unibo.it>
2021-09-12 16:29:17 +0000

    Update ConnectionListener.java

--- com/company/server/ConnectionListener.java
+++ com/company/server/ConnectionListener.java
@@ -3,28 +3,26 @@ package com.company.server;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.gui2.Label;
 import com.googlecode.lanterna.gui2.Panel;
+
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
 import java.util.Map;
 
 import static com.googlecode.lanterna.TextColor.ANSI.BLACK;
 
 // Questa classe si occupa di rimanere in attesa di nuove connessioni da parte dei client
 public class ConnectionListener implements Runnable {
-    private HashMap<String, PrintWriter> connectedClients;
     private Panel panel;
     private TextColor coloreLabel;
     public static Thread handlerThread;
     private int SERVERPORT;
     private String players = "";
 
-    public ConnectionListener(Panel panel, int SERVERPORT, TextColor coloreLabel, HashMap connectedClients) {
+    public ConnectionListener(Panel panel, int SERVERPORT, TextColor coloreLabel) {
         this.panel=panel;
         this.SERVERPORT = SERVERPORT;
         this.coloreLabel=coloreLabel;
-        this.connectedClients=connectedClients;
     }
 
     @Override
@@ -34,25 +32,25 @@ public class ConnectionListener implements Runnable {
             listener = new ServerSocket(SERVERPORT);
             listener.setReuseAddress(true);
 
-            while(connectedClients.size() <= 4){
-                System.out.println(connectedClients.size());
+            while(Server.connectedClients.size() <= 4){
+                System.out.println(Server.connectedClients.size());
                 Socket socket = listener.accept();
                 // Per ogni client che si connette al server creo un thread handlerThread,
                 // a lato server, per ogni client così che possano essere gestiti singolarmente dal server
-                ClientHandler clientSock = new ClientHandler(socket, "",panel,coloreLabel,connectedClients);
+                ClientHandler clientSock = new ClientHandler(socket, "",panel,coloreLabel);
                 clientSock.clientHandler();
                 handlerThread = new Thread(clientSock);
                 handlerThread.start();
 
-                Label lab_clientJoin=new Label("[SERVER]: Connected clients: " + (connectedClients.size())+ "/4").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
+                Label lab_clientJoin=new Label("[SERVER]: Connected clients: " + (Server.connectedClients.size())+ "/4").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
                 panel.addComponent(lab_clientJoin);
 
                 // Aggiorno i vari client che un nuovo giocatore si è connesso al server
-                broadcastServerMessage("[SERVER]: Connected clients: " + (connectedClients.size()) + "/4");
-                if(connectedClients.size() == 4){
+                broadcastServerMessage("[SERVER]: Connected clients: " + (Server.connectedClients.size()) + "/4");
+                if(Server.connectedClients.size() == 4){
                     Server.gameStarted = true;
                     broadcastServerMessage("/start");
-                    for (Map.Entry<String, PrintWriter> pair : connectedClients.entrySet()) {
+                    for (Map.Entry<String, PrintWriter> pair : Server.connectedClients.entrySet()) {
                         players = players + pair.getKey() + "-";
                     }
                     broadcastServerMessage(players);
@@ -71,7 +69,7 @@ public class ConnectionListener implements Runnable {
 
     public void broadcastServerMessage(String message) {
 
-        for(Map.Entry<String, PrintWriter> e : connectedClients.entrySet()) {
+        for(Map.Entry<String, PrintWriter> e : Server.connectedClients.entrySet()) {
 
             e.getValue().println(message);
             e.getValue().flush();
