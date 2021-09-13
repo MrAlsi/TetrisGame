2c52a0354e59bdc1e8c9260ae4b65a2dd5c2882a
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-09-13 17:07:56 +0000

    Update InviaStato.java

--- com/company/Gioco/InviaStato.java
+++ com/company/Gioco/InviaStato.java
@@ -1,14 +1,15 @@
 package com.company.Gioco;
 
 import com.googlecode.lanterna.TextColor;
-
 import java.io.PrintWriter;
+import java.util.concurrent.Semaphore;
 
 public class InviaStato implements Runnable{
     int[][] miaGriglia = new int[12][24];
     PrintWriter pw;
     String username;
     TextColor colore;
+    Semaphore traduzione = new Semaphore(1);
 
     public InviaStato(String username, PrintWriter pw){
         this.pw=pw;
@@ -25,24 +26,31 @@ public class InviaStato implements Runnable{
     }
 
     public synchronized void traduciGrigliaToInt(Griglia griglia){
-        for(int i=0; i<griglia.griglia.length; i++){
-            for(int e=0; e<griglia.griglia[i].length; e++){
-                switch (griglia.griglia[i][e].getStato()) {
-                    case 0 :{miaGriglia[i][e]=0;
-                        break;}
-                    case 1 :{miaGriglia[i][e]=1;
+        try {
+            traduzione.acquire();
+            for(int i=0; i<griglia.griglia.length; i++){
+                for(int e=0; e<griglia.griglia[i].length; e++){
+                    switch (griglia.griglia[i][e].getStato()) {
+                        case 0 :{miaGriglia[i][e]=0;
+                            break;}
+                        case 1 :{miaGriglia[i][e]=1;
                             colore=griglia.griglia[i][e].colore;
-                        break;}
-                    case 2 :{miaGriglia[i][e]=2;
-                        break;}
-                    case 3 : {
-                        miaGriglia[i][e] = 3;
-                        break;
+                            break;}
+                        case 2 :{miaGriglia[i][e]=2;
+                            break;}
+                        case 3 : {
+                            miaGriglia[i][e] = 3;
+                            break;
+                        }
                     }
                 }
             }
+            traduciInttoString(miaGriglia);
+            traduzione.release();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        traduciInttoString(miaGriglia);
+
     }
 
     public synchronized void traduciInttoString(int[][] mat){
