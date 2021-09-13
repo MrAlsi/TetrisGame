bb913b23fefb247bc8472abfd58894a95df18c4c
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-09-13 16:28:02 +0000

    Update PezzoL.java

--- com/company/Gioco/Pezzi/PezzoL.java
+++ com/company/Gioco/Pezzi/PezzoL.java
@@ -29,16 +29,14 @@ public class PezzoL extends Pezzo {
     public void ruota(Griglia campo, int rotazione, int verso){
 
         for(int i=3; i>=0; i--){
-            if ((pezzo[0].rigaGriglia > 1) && (pezzo[0].colonnaGriglia < 11) &&
-                (pezzo[1].rigaGriglia > 1) && (pezzo[1].colonnaGriglia < 11) && (pezzo[1].colonnaGriglia > 0)) {
-                    pezzo[i].rimuovi(campo, pezzo[i].getColonna(), pezzo[i].getRiga());
+            pezzo[i].rimuovi(campo, pezzo[i].getColonna(), pezzo[i].getRiga());
         /* }
         for(int i=3;i>=0;i--){*/
                     pezzo[i].muovi(campo, pezzo[i].getColonna(), pezzo[i].getRiga(), spostamentoOrizzontale[rotazione][i], spostamentoVerticale[rotazione][i], colore);
                     pezzo[i].colonnaGriglia = pezzo[i].getColonna() + spostamentoOrizzontale[rotazione][i];
                     pezzo[i].rigaGriglia = pezzo[i].getRiga() + spostamentoVerticale[rotazione][i];
 
-            }
+            
         }
         if(rotazione!=maxRotazioni)
             this.rotazione++;
