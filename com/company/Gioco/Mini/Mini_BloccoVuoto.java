b3f25c254723376caa19d010b73228e9fba5296c
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-09-13 14:41:32 +0000

    Update Mini_BloccoVuoto.java

--- com/company/Gioco/Mini/Mini_BloccoVuoto.java
+++ com/company/Gioco/Mini/Mini_BloccoVuoto.java
@@ -6,7 +6,7 @@ import com.googlecode.lanterna.graphics.TextGraphics;
 
 public class Mini_BloccoVuoto extends Mini_Blocco {
     public Mini_BloccoVuoto(TextGraphics screen, int col, int rig, int scostamento) {
-        super(screen, col, rig, TextColor.ANSI.BLACK_BRIGHT, scostamento);
+        super(screen, col, rig, TextColor.ANSI.BLACK_BRIGHT, Symbols.BLOCK_SOLID, scostamento);
         stato = 0;
     }
 }
