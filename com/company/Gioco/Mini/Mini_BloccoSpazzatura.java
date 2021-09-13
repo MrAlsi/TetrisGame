6bead24085ee7322778eafa1ed79c9e9a02306f4
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-09-13 14:39:47 +0000

    Update Mini_BloccoSpazzatura.java

--- com/company/Gioco/Mini/Mini_BloccoSpazzatura.java
+++ com/company/Gioco/Mini/Mini_BloccoSpazzatura.java
@@ -5,7 +5,7 @@ import com.googlecode.lanterna.graphics.TextGraphics;
 
 public class Mini_BloccoSpazzatura extends Mini_Blocco {
     public Mini_BloccoSpazzatura(TextGraphics screen, int colGriglia, int rigGriglia,  int scostamento){
-        super(screen, colGriglia, rigGriglia, TextColor.ANSI.BLACK,scostamento);
+        super(screen, colGriglia, rigGriglia, TextColor.ANSI.RED, Symbols.BLOCK_SPARSE, scostamento);
         stato=3;
     }
 }
