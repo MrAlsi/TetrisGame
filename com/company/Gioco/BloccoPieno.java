6b1d1f3d35adf89f4499d15aba5f7f9d48314d34
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-09-19 08:52:27 +0000

    Update BloccoPieno.java

--- com/company/Gioco/BloccoPieno.java
+++ com/company/Gioco/BloccoPieno.java
@@ -1,25 +1,14 @@
 package com.company.Gioco;
 
 import com.googlecode.lanterna.TextColor;
-
-
 import com.googlecode.lanterna.graphics.TextGraphics;
-
 import static com.googlecode.lanterna.Symbols.BLOCK_SOLID;
 
 public class BloccoPieno extends Blocco{
-    public static int velocita = 1000;
 
     public BloccoPieno(TextGraphics screen, int colTerminale, int rigTerminale, TextColor colore) {
         super(screen, colTerminale, rigTerminale, colore, BLOCK_SOLID);
         stato=1;
-    }
-
-    public void cambiaColore(){ }
-
-    //La velocità aumenta quando
-    public void aumentaVelocita(){
-        velocita=velocita-100;
     }
 
     //orizzontale: +1: sposta a destra  -1:sposta a sinistra
@@ -27,27 +16,16 @@ public class BloccoPieno extends Blocco{
 
     @Override
     public void muovi(Griglia campo, int colGriglia, int rigGriglia, int orizzontale, int verticale, TextColor colore){
-
         //Riempo la successiva con un bloccoPieno
         campo.griglia[colGriglia+orizzontale][rigGriglia+verticale] =
                 new BloccoPieno(schermo,
                         campo.griglia[colGriglia+orizzontale][rigGriglia+verticale].getColonna(),
                         campo.griglia[colGriglia+orizzontale][rigGriglia+verticale].getRiga(), colore);
-
     }
 
     public void rimuovi(Griglia campo, int colGriglia, int rigGriglia){
         //Metto la casella dove sono stato come blocco vuoto
         campo.griglia[colGriglia][rigGriglia] = new BloccoVuoto(schermo, colGriglia, rigGriglia);
-    }
-
-    //Gestione delle collisioni
-    public boolean collisioneLaterale(Griglia campo, int x, int y, int spostamento){
-        if(x==0 || x==11 || campo.griglia[x+spostamento][y].getStato() == 2){
-            return true;
-        } else {
-            return false;
-        }
     }
 
     @Override
