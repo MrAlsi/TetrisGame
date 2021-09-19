94bca00874c89a212a8581b5f725623714db04cc
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-09-19 08:32:47 +0000

    Update Griglia.java

--- com/company/Gioco/Griglia.java
+++ com/company/Gioco/Griglia.java
@@ -1,6 +1,7 @@
 package com.company.Gioco;
 
 import com.googlecode.lanterna.graphics.TextGraphics;
+import static com.Gioco.Schermo.*;
 
 /**
  * La classe griglia è la classe che crea e gestisce il campo da gioco, è costituita da una griglia 12x24 di @Blocco
@@ -16,7 +17,6 @@ public class Griglia {
         this.screen = screen;
         griglia = new Blocco[12][24];
     }
-
 
     /**
      * Metodo che crea un campo costituito da BlocchiVuoti
@@ -71,6 +71,10 @@ public class Griglia {
         }
     }
 
+    /**
+     * @param riga
+     * fa cadere la struttura dalla riga passa come parametro
+     */
     public void cadutaStruttura(int riga){
         for(int i = riga - 1; i >= 0; i--){
             for(int j = 0; j < 12; j++){
@@ -91,37 +95,62 @@ public class Griglia {
      * @param righeSpazzatura
      */
     public void aggiungiSpazzatura(int righeSpazzatura){
-        for(int i = 4; i < 24; i++){
+        //Alza il pezzo, non posso trattarlo come un blocco normale perché altrimenti, al brickDropTimer scenderebbe da
+        //dov'era prima
+        pezzoScelto.alzaPezzo(righeSpazzatura);
+
+        for(int i = 0; i < 24; i++){
+            //if(i >= righeSpazzatura - 1)
             for(int j = 0; j < 12; j++){
-                if(griglia[j][i].stato==2 && i >= righeSpazzatura - 1) {
-                    griglia[j][i - righeSpazzatura] = new BloccoStruttura(screen,
-                            griglia[j][i - righeSpazzatura].colonnaGriglia,
-                            griglia[j][i - righeSpazzatura].rigaGriglia);
+                switch(griglia[j][i].stato) {
+                    //Devo alzare un blocco vuoto
+                    case 0: {
+                        if (!(i - righeSpazzatura < 0)) //Controllo che non vada oltre la griglia per evitare un errore
+                            griglia[j][i - righeSpazzatura] = new BloccoVuoto(screen,
+                                    griglia[j][i - righeSpazzatura].colonnaGriglia,
+                                    griglia[j][i - righeSpazzatura].rigaGriglia);
+                        break;
+                    }
+
+                    //Devo alzare un blocco della struttura
+                    case 2: {
+                        if (i - righeSpazzatura < 0){ //Controllo che se va oltre la griglia, nel caso ho perso
+                            haiPerso();
+                            System.out.println("2");
+                            break;
+                        } else {
+                            griglia[j][i - righeSpazzatura] = new BloccoStruttura(screen,
+                                    griglia[j][i - righeSpazzatura].colonnaGriglia,
+                                    griglia[j][i - righeSpazzatura].rigaGriglia);
+                        }
+                        break;
+                    }
+
+                    //Devo alzare della spazzatura
+                    case 3: {
+                        if (i - righeSpazzatura < 0) { //Controllo che se va oltre la griglia, nel caso ho perso
+                            haiPerso();
+                            System.out.println("3");
+                            break;
+                        } else {
+                            griglia[j][i - righeSpazzatura] = new BloccoSpazzatura(screen,
+                                    griglia[j][i - righeSpazzatura].colonnaGriglia,
+                                    griglia[j][i - righeSpazzatura].rigaGriglia);
+                        }
+                        break;
+
+                    }
                 }
-                if(griglia[j][i].stato==0 && i >= righeSpazzatura - 1) {
-                    griglia[j][i - righeSpazzatura] = new BloccoVuoto(screen,
-                            griglia[j][i - righeSpazzatura].colonnaGriglia,
-                            griglia[j][i - righeSpazzatura].rigaGriglia);
-                }
-                if(griglia[j][i].stato==3 && i >= righeSpazzatura - 1) {
-                    griglia[j][i - righeSpazzatura] = new BloccoSpazzatura(screen,
-                            griglia[j][i - righeSpazzatura].colonnaGriglia,
-                            griglia[j][i - righeSpazzatura].rigaGriglia);
-                }
-                if(i >= (23 - righeSpazzatura)){
+
+                //Disegno le nuove righe spazzatura in fondo
+                if(i >= (23 - righeSpazzatura)) {
                     griglia[j][i] = new BloccoSpazzatura(screen,
                             griglia[j][i].colonnaGriglia,
                             griglia[j][i].rigaGriglia);
                 }
             }
-        }
-    }
 
-    //Un po' una maialata, però cosi si perde
-    public boolean sconfitta(){
-        if(griglia[4][0].stato>1||griglia[5][0].stato>1||griglia[6][0].stato>1||griglia[3][0].stato>1){
-            return true;
         }
-        return false;
+
     }
 }
