817dc6e4f944d77c2a129a184eec571fc129135a
carlotta carboni <carlotta.carboni2@studio.unibo.it>
2021-09-20 13:32:44 +0000

    massimo label.java

--- com/company/server/ConnectionListener.java
+++ com/company/server/ConnectionListener.java
@@ -3,12 +3,14 @@ package com.company.server;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.gui2.Label;
 import com.googlecode.lanterna.gui2.Panel;
+import com.googlecode.lanterna.gui2.TextBox;
 
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Map;
 
+import static com.company.server.Server.ip;
 import static com.googlecode.lanterna.TextColor.ANSI.BLACK;
 
 // Questa classe si occupa di rimanere in attesa di nuove connessioni da parte dei client
@@ -18,22 +20,23 @@ public class ConnectionListener implements Runnable {
     public static Thread handlerThread;
     private int SERVERPORT;
     private String players = "";
+    public TextBox messaggio=new TextBox();
 
-    public ConnectionListener(Panel panel, int SERVERPORT, TextColor coloreLabel) {
+    public  ConnectionListener(Panel panel, int SERVERPORT, TextColor coloreLabel) {
         this.panel=panel;
         this.SERVERPORT = SERVERPORT;
         this.coloreLabel=coloreLabel;
     }
 
     @Override
-    public void run() {
+    public synchronized void run() {
         try{
             ServerSocket listener = null;
             listener = new ServerSocket(SERVERPORT);
             listener.setReuseAddress(true);
 
             while(Server.connectedClients.size() <= 4){
-                System.out.println("Client connessi" + Server.connectedClients.size() + "/4");    
+                System.out.println("Client connessi" + Server.connectedClients.size() + "/4");
                 Socket socket = listener.accept();
                 // Per ogni client che si connette al server creo un thread handlerThread,
                 // a lato server, per ogni client così che possano essere gestiti singolarmente dal server
@@ -43,6 +46,7 @@ public class ConnectionListener implements Runnable {
                 handlerThread.start();
 
                 Label lab_clientJoin=new Label("[SERVER]: Connected clients: " + (Server.connectedClients.size())+ "/4").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
+                controlloLabel(messaggio);
                 panel.addComponent(lab_clientJoin);
 
                 // Aggiorno i vari client che un nuovo giocatore si è connesso al server
@@ -57,6 +61,7 @@ public class ConnectionListener implements Runnable {
                     broadcastServerMessage(players);
                     System.out.println("Il gioco è iniziato!");
                     Label lab_serverMsg = new Label("[SERVER]: The game has started!").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
+                    controlloLabel(messaggio);
                     panel.addComponent(lab_serverMsg);
                     while(Server.gameStarted){
 
@@ -76,5 +81,21 @@ public class ConnectionListener implements Runnable {
             e.getValue().println(message);
             e.getValue().flush();
         }
+    }
+    public synchronized void controlloLabel(TextBox messaggio){
+        if (Server.contaLabel>=15){
+            Server.contaLabel=0;
+            panel.removeAllComponents();
+            Label lab=new Label("\nStarting "+ ServerSender.name + "... ").setBackgroundColor(BLACK).setForegroundColor(
+                    coloreLabel);
+            panel.addComponent(lab);
+            Label myIp = new Label("\nShare your ip address: " + ip).setBackgroundColor(BLACK).setForegroundColor(
+                    coloreLabel);
+            panel.addComponent(myIp);
+            Label lab_serverOn=new Label("\n- - - Server on - - -").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
+            panel.addComponent(lab_serverOn);
+            panel.addComponent(messaggio);
+        }
+        Server.contaLabel++;
     }
 }
