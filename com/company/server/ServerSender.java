b930deede2cab2418e25744a525e1013954bd6d9
carlotta carboni <carlotta.carboni2@studio.unibo.it>
2021-09-20 13:31:32 +0000

    massimo label.java

--- com/company/server/ServerSender.java
+++ com/company/server/ServerSender.java
@@ -1,5 +1,6 @@
 package com.company.server;
 
+import com.company.client.Client;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.gui2.Button;
 import com.googlecode.lanterna.gui2.Label;
@@ -19,6 +20,8 @@ public class ServerSender implements Runnable{
     private TextColor coloreLabel;
     public static String name;
     private String players = "";
+    public String ip;
+
 
     public ServerSender(Panel panel, TextColor coloreLabel, String name) {
         this.name=name;
@@ -26,10 +29,11 @@ public class ServerSender implements Runnable{
         this.coloreLabel=coloreLabel;
     }
 
-    public void run() {
+    public synchronized void run() {
 
         TextBox messaggio=new TextBox();
         panel.addComponent(messaggio);
+
         inviaMessaggio(messaggio);
 
     }
@@ -48,16 +52,18 @@ public class ServerSender implements Runnable{
                     // Se il server invia il messaggio "/start" il gioco ccerca di partire
 
                     if (messaggioString.equals("/start")) {
-                          System.out.println("Il Server ha dato il comando START : il gioco cerca di partire...");
+                        System.out.println("Il Server ha dato il comando START : il gioco cerca di partire...");
                         //controllo il numero dei giocatori
                         if (Server.connectedClients.size() < 2) {
-                           System.out.println("Numero di giocatori insufficiente");
+                            System.out.println("Numero di giocatori insufficiente");
                             Label lab_serverMsg = new Label("[SERVER]: Numero di giocatori insufficiente.")
                                     .setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
-                            panel.addComponent(lab_serverMsg);
+                            controlloLabel(messaggio,lab_serverMsg,messaggioString);
+
 
                         } else {
                             //svuoto il pannello e avverto i client
+                            System.out.println("Partita iniziata!");
                             broadcastServerMessage(messaggioString);
                             for (Map.Entry<String, PrintWriter> pair : Server.connectedClients.entrySet()) {
                                 players = players + pair.getKey() + "-";
@@ -65,12 +71,12 @@ public class ServerSender implements Runnable{
                             broadcastServerMessage(players);
                             players = "";
                             Server.gameStarted = true;
-                            System.out.println("Partita iniziata!");
                             Label lab_serverMsg = new Label("[SERVER]: Partita iniziata").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
-                            panel.addComponent(lab_serverMsg);
+                            controlloLabel(messaggio,lab_serverMsg,messaggioString);
+
                         }
                     } else if (messaggioString.equals("/quit")) {
-                            System.out.println("Uscita dal gioco");
+                        System.out.println("Uscita dal gioco");
                         if(Server.connectedClients.size()==0){
                             Server.serverThread.stop();
                             System.exit(0);
@@ -86,16 +92,13 @@ public class ServerSender implements Runnable{
                     } else if(messaggioString.equals("/restart")) {
                         System.out.println("Restart...");
                         Label lab_serverMsg = new Label("[SERVER]: Resettando la partita...").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
-                        panel.addComponent(lab_serverMsg);
-                        broadcastServerMessage("[" + name + "]: " + messaggioString);
+                        controlloLabel(messaggio,lab_serverMsg,messaggioString);
 
                     } else {
                         // In tutti gli altri casi trasmetto il messaggio del server a tutti i client connessi
-                                System.out.println("trasmetto il messaggio del server a tutti i client connessi");
+                        System.out.println("trasmetto il messaggio del server a tutti i client connessi");
                         Label lab_serverMsg = new Label("[" + name + "]: " + messaggioString).setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
-                        panel.addComponent(lab_serverMsg);
-
-                        broadcastServerMessage("[" + name + "]: " + messaggioString);
+                        controlloLabel(messaggio,lab_serverMsg,messaggioString);
                     }
 
                     // Una volta inviato il messaggio pulisco la textbox
@@ -103,6 +106,26 @@ public class ServerSender implements Runnable{
                 }
             }
         }).addTo(panel);
+    }
+    public synchronized void controlloLabel(TextBox messaggio, Label lab_serverMsg,String messaggioString){
+        if (Server.contaLabel>=15){
+            Server.contaLabel=0;
+            panel.removeAllComponents();
+            Label lab=new Label("\nStarting "+ name + "... ").setBackgroundColor(BLACK).setForegroundColor(
+                    coloreLabel);
+            panel.addComponent(lab);
+            Label myIp = new Label("\nShare your ip address: " + Server.ip).setBackgroundColor(BLACK).setForegroundColor(
+                    coloreLabel);
+            panel.addComponent(myIp);
+            Label lab_serverOn=new Label("\n- - - Server on - - -").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
+            panel.addComponent(lab_serverOn);
+            panel.addComponent(messaggio);
+            inviaMessaggio(messaggio);
+
+        }
+        Server.contaLabel++;
+        panel.addComponent(lab_serverMsg);
+        broadcastServerMessage("[" + name + "]: " + messaggioString);
     }
     public void broadcastServerMessage(String message) {
 
