33005ab7811cbc9b0dada0531971dd64a9c8bdac
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-09-21 16:07:04 +0000

    aggiunti commenti

--- com/company/Gioco/Mini/Mini_BloccoStruttura.java
+++ com/company/Gioco/Mini/Mini_BloccoStruttura.java
@@ -6,8 +6,14 @@ import com.googlecode.lanterna.graphics.TextGraphics;
 import static com.googlecode.lanterna.Symbols.BLOCK_SOLID;
 
 public class Mini_BloccoStruttura extends Mini_Blocco {
-    public Mini_BloccoStruttura(TextGraphics schermo, int col, int rig, int scostamento) {
-        super(schermo, col, rig, TextColor.ANSI.WHITE_BRIGHT, BLOCK_SOLID, scostamento);
+    public Mini_BloccoStruttura(TextGraphics screen, int colGriglia, int rigGriglia, int scostamento) {
+        super(screen,
+                colGriglia,             //Colonna nella griglia
+                rigGriglia,             //Riga nella griglia
+                TextColor.ANSI.WHITE,   //Colore del pezzo che sta scendendo
+                BLOCK_SOLID,            //Feature del blocco
+                scostamento);           //Lo scostamento è lo scostamento dal margine sinistro dello schermo, a seconda
+                                        //dello scostamento sai in che campo devi cambiare
         stato = 2;
     }
 }
