bebfa9bc0fdc1b68fdb2776faea682e92c1c06d8
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-09-21 13:18:49 +0000

    aggiunti commenti e javaDoc

--- com/company/Gioco/Blocco.java
+++ com/company/Gioco/Blocco.java
@@ -1,22 +1,28 @@
 package com.company.Gioco;
 
-import com.googlecode.lanterna.Symbols;
 import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 
+/**
+ * Un blocco è un quadrato del campo da gioco, ogni blocco può essere di quattro tipi:
+ * blocco vuoto è un quadrato ancora libero.
+ * blocco pieno è un quadrato del pezzo che sta scendendo.
+ * blocco struttura è un quadrato occupato da un pezzo che ha avuto una collisione sotto e che aspetta di essere eliminato
+ * blocco spazzatura è un quadrato di una riga spazzatura mandato da un altro giocatore
+ *
+ */
 public class Blocco {
 
     TextGraphics schermo; // Griglia nel terminale
-    // TextGraphics quadrato; // Quadratino nel terminale
-    private int coefColonna = 4; // Numero di quadratini che formano la larghezza di un Blocco nel terminale
-    private int coefRiga = 2; // Numero di quadratini che formano l'altezza di un Blocco nel terminale
+    private final int coefColonna = 4; // Numero di quadratini che formano la larghezza di un Blocco nel terminale
+    private final int coefRiga = 2; // Numero di quadratini che formano l'altezza di un Blocco nel terminale
 
     TextColor colore;
     public int colonnaGriglia;
     public int rigaGriglia;
-    int stato = 3; // 0=Vuoto - 1=Pezzo che sta scendendo - 2=Struttura  - 3=Spazzattura
+    int stato; // 0=Vuoto - 1=Pezzo che sta scendendo - 2=Struttura  - 3=Spazzattura
 
     public Blocco(TextGraphics schermo, int colonnaGriglia, int rigaGriglia, TextColor colore, char simbolo) {
         this.schermo = schermo;
@@ -33,29 +39,51 @@ public class Blocco {
         }
     }
 
-    // Ritorna la posizione della colonna sullo schermo, coordinata * coefficente
+    /**
+     * @return la posizione della colonna sulla griglia
+     */
     public int getColonna() { 
         return colonnaGriglia; 
     }
 
-    // Ritorna la posizione della riga sullo schermo, coordinata * coefficente
+    /**
+     * @return la posizione della riga sullo schermo, coordinata * coefficente
+     */
     public int getRiga() {
         return rigaGriglia;
     }
 
-    // Ritorna il colore del blocco
+    /**
+     * @return il colore del blocco
+     */
     public TextColor getColor() {
         return colore;
     }
 
+    /**
+     * @return lo stato del blocco
+     */
     public int getStato() {
         return stato;
     }
 
+    /**
+     * Controlla se il blocco si trova all'ultima riga della griglia oppure se nel blocco sotto di lui (this.getRiga() + 1)
+     * si trova un blocco con stato > 1 (Struttura=2 o Spazzatura=3)
+     * @return True: se c'è qualcosa sotto       False: non c'è ancora spazio per scendere
+     */
     public boolean collisioneSotto(Griglia campo) {
         return this.getRiga() == 23 || campo.griglia[this.getColonna()][this.getRiga() + 1].getStato() > 1;
     }
 
+    /**
+     * Controlla se con lo spostamento in orizzontale andrà fuori della griglia in larghezza, se con lo spostamento
+     * in verticale andrà fuori in altezza o se dove andrà c'è già un altro blocco (struttura o spazzatura)
+     * @param campo
+     * @param spostamentoOrizzontale
+     * @param spostamentoVerticale
+     * @return
+     */
     public boolean collisioneLaterale(Griglia campo, int spostamentoOrizzontale, int spostamentoVerticale) {
         return this.getColonna() + spostamentoOrizzontale < 0 || //Collisione con muro di sinistra
                 this.getColonna() + spostamentoOrizzontale > 11 || //Collisione con muro di destra
