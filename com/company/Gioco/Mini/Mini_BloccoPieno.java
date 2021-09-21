b8054713b9b8df773879b7c3ed1f70d66a9f3922
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-09-21 16:08:50 +0000

    aggiunti commenti

--- com/company/Gioco/Mini/Mini_BloccoPieno.java
+++ com/company/Gioco/Mini/Mini_BloccoPieno.java
@@ -7,7 +7,13 @@ import static com.googlecode.lanterna.Symbols.BLOCK_SOLID;
 
 public class Mini_BloccoPieno extends Mini_Blocco {
     public Mini_BloccoPieno(TextGraphics screen, int colGriglia, int rigGriglia, TextColor colore, int scostamento){
-        super(screen, colGriglia, rigGriglia, colore, BLOCK_SOLID, scostamento);
+        super(screen,
+                colGriglia,         //Colonna nella griglia
+                rigGriglia,         //Riga nella griglia
+                colore,             //Colore del pezzo che sta scendendo
+                BLOCK_SOLID,        //Feature del blocco
+                scostamento);       //Lo scostamento è lo scostamento dal margine sinistro dello schermo, a seconda
+                                    //dello scostamento sai in che campo devi cambiare
         stato=1;
     }
 }
