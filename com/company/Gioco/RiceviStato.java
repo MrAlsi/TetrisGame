b62cf78637a72911a36190e9e7efa35632398685
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-09-21 16:03:39 +0000

    aggiunti commenti e javaDoc

--- com/company/Gioco/RiceviStato.java
+++ com/company/Gioco/RiceviStato.java
@@ -1,12 +1,13 @@
 package com.company.Gioco;
 
-import com.company.Gioco.Mini.*;
 import com.googlecode.lanterna.TextColor;
 import java.util.concurrent.Semaphore;
-
+import com.company.Gioco.Mini.*;
 import static com.company.Gioco.Schermo.*;
 
-
+/**
+ * serve per ricevere lo stato della griglia degli altri giocati e tradurlo nei minicampi
+ */
 public class RiceviStato extends Thread{
     public static Semaphore traduzione = new Semaphore(1);
 
@@ -14,6 +15,16 @@ public class RiceviStato extends Thread{
         traduciStringToInt(stringa);
     }
 
+    /**
+     * @param stringa ricevuta dagli altri giocatori, splittiamo la stringa in tre parti ogni ':'
+     *                [0]: indica il giocatore che ci ha inviato la stringa, è colui che dobbiamo modificare il mini_Campo
+     *                [1]: lo stato della sua griglia in quel momento
+     *                [2]: il colore del pezzo che sta scendendo in quel momento.
+     *                la Stringa [1] la splittiamo per ogni elemento e lo mettiamo nell'array blocchi, ogni elemento di
+     *                questo array lo 'castiamo' lo mettiamo dentro una matrice[12][24] di interi.
+     *                Richiamo il metodo getColore
+     *                Traduco la matrice di interi in una matrice di mini_Blocco
+     */
     public void traduciStringToInt (String stringa){
         try {
             traduzione.acquire();
@@ -36,17 +47,24 @@ public class RiceviStato extends Thread{
         }
     }
 
+    /**
+     * @param campo matrice di interi che ci indica lo stato del blocco
+     * @param nome nome del giocatore a cui dobbiamo aggiornare il campo
+     * @param colore colore del pezzo che sta scendendo in quel momento
+     *
+     * faccio un for per trovare il numero del campo il cui giocatore ha quel nome, dopo di che per ogni elemento della
+     * griglia di interi lo traduco nel suo corrispondente in mini_Blocco
+     */
     public void traduciIntToMiniGriglia(int[][] campo, String nome, TextColor colore) {
-
-            int id = 0;
-
-            for (int i = 0; i < miniCampo.length; i++) {
+            int id = 0;                                     //Id del campo del giocatore
+            for (int i = 0; i < miniCampo.length; i++) {    //For per trovare il giocatore con quel nome
                 if (nome.equals(miniCampo[i].nome)) {
                     id = i;
                     break;
                 }
             }
 
+            //Traduttore da interi a mini_Blocco
             for (int i = 0; i < campo.length; i++) {
                 for (int e = 0; e < campo[i].length; e++) {
                     switch (campo[i][e]) {
@@ -70,6 +88,9 @@ public class RiceviStato extends Thread{
 
     }
 
+    /**
+     * Venendomi passata una Stringa all'inizio faccio uno Switch sulla stringa per ritornare il corrispettivo TextColor
+     */
     public TextColor getColore(String colore){
         switch(colore){
             case "BLUE_BRIGHT": return TextColor.ANSI.BLUE_BRIGHT;
@@ -79,7 +100,6 @@ public class RiceviStato extends Thread{
             case "CYAN_BRIGHT": return TextColor.ANSI.CYAN_BRIGHT;
             case "GREEN_BRIGHT": return TextColor.ANSI.GREEN_BRIGHT;
         }
-
         return null;
     }
 }
