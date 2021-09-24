058fb1ac74d41e0bc3a9eabd6b057bdaac6da841
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-09-24 11:09:42 +0000

    rotazioni antiorario.java

--- com/company/Gioco/Pezzi/PezzoLungo.java
+++ com/company/Gioco/Pezzi/PezzoLungo.java
@@ -1,8 +1,8 @@
-package com.company.Gioco.Pezzi;
+package com.Gioco.Pezzi;
 
-import com.company.Gioco.BloccoPieno;
-import com.company.Gioco.Griglia;
-import com.company.Gioco.Pezzo;
+import com.Gioco.BloccoPieno;
+import com.Gioco.Griglia;
+import com.Gioco.Pezzo;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 
@@ -31,16 +31,20 @@ public class PezzoLungo extends Pezzo {
             pezzo[i].rimuovi(campo, pezzo[i].getColonna(), pezzo[i].getRiga());
         /* }
         for(int i=3;i>=0;i--){*/
-            pezzo[i].muovi(campo, pezzo[i].getColonna(), pezzo[i].getRiga(), spostamentoOrizzontale[rotazione][i], spostamentoVerticale[rotazione][i], colore);
-            pezzo[i].colonnaGriglia = pezzo[i].getColonna() + spostamentoOrizzontale[rotazione][i];
-            pezzo[i].rigaGriglia = pezzo[i].getRiga() + spostamentoVerticale[rotazione][i];
+            pezzo[i].muovi(campo, pezzo[i].getColonna(), pezzo[i].getRiga(), spostamentoOrizzontale[rotazione][i]*verso, spostamentoVerticale[rotazione][i]*verso, colore);
+            pezzo[i].colonnaGriglia = pezzo[i].getColonna() + spostamentoOrizzontale[rotazione][i]*verso;
+            pezzo[i].rigaGriglia = pezzo[i].getRiga() + spostamentoVerticale[rotazione][i]*verso;
 
         }
 
-        if(rotazione!=maxRotazioni)
-            this.rotazione++;
-        else
-            this.rotazione=0;
+        if(verso!=-1) {
+            if (rotazione != maxRotazioni) {
+                this.rotazione++;
+            } else {
+                this.rotazione = 0;
+            }
+        } else
+            this.rotazione=rotazione;
     }
 }
 
