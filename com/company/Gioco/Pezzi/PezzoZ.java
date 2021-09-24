5e45585fac8586b107e8562a8b881c8d087c1ce8
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-09-24 11:13:23 +0000

    rotazione antiorario.java

--- com/company/Gioco/Pezzi/PezzoZ.java
+++ com/company/Gioco/Pezzi/PezzoZ.java
@@ -16,12 +16,12 @@ public class PezzoZ extends Pezzo{
                 new BloccoPieno(schermo, 4, 1, TextColor.ANSI.GREEN_BRIGHT),
                 new BloccoPieno(schermo, 5, 1, TextColor.ANSI.GREEN_BRIGHT));
         tipoPezzo = 5;
-        rotazione = 0;
+        rotazione = 1;
 
         maxRotazioni = 1;
 
-        spostamentoVerticale= new int[][]{{2,0,0,0}, {-2,0,0,0}};
-        spostamentoOrizzontale = new int[][]{{1,1,0,0}, {-1,-1,0,0}};
+        spostamentoVerticale= new int[][]{{1,0,1,0}, {-1,0,-1,0}};
+        spostamentoOrizzontale = new int[][]{{-1,0,1,2}, {1,0,-1,-2}};
 
     }
 
@@ -31,14 +31,18 @@ public class PezzoZ extends Pezzo{
            pezzo[i].rimuovi(campo, pezzo[i].getColonna(), pezzo[i].getRiga());
        }
        for(int i=3;i>=0;i--){
-           pezzo[i].muovi(campo, pezzo[i].getColonna(), pezzo[i].getRiga(), spostamentoOrizzontale[rotazione][i], spostamentoVerticale[rotazione][i], pezzo[i].getColor());
-           pezzo[i].colonnaGriglia = pezzo[i].getColonna() + spostamentoOrizzontale[rotazione][i];
-           pezzo[i].rigaGriglia = pezzo[i].getRiga() + spostamentoVerticale[rotazione][i];
+           pezzo[i].muovi(campo, pezzo[i].getColonna(), pezzo[i].getRiga(), spostamentoOrizzontale[rotazione][i]*verso, spostamentoVerticale[rotazione][i]*verso, colore);
+           pezzo[i].colonnaGriglia = pezzo[i].getColonna() + spostamentoOrizzontale[rotazione][i]*verso;
+           pezzo[i].rigaGriglia = pezzo[i].getRiga() + spostamentoVerticale[rotazione][i]*verso;
        }
 
-       if(rotazione!=maxRotazioni)
-         this.rotazione++;
-       else
-         this.rotazione=0;
+        if(verso!=-1) {
+            if (rotazione != maxRotazioni) {
+                this.rotazione++;
+            } else {
+                this.rotazione = 0;
+            }
+        } else
+            this.rotazione=rotazione;
     }
 }
