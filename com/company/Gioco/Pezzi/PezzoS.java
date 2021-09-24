d73505e78bed317d1e606a3d14af81449e38779e
Gabriel Alsina <gabriel.alsina22@gmail.com>
2021-09-24 11:11:02 +0000

    rotazioni antiorario.java

--- com/company/Gioco/Pezzi/PezzoS.java
+++ com/company/Gioco/Pezzi/PezzoS.java
@@ -1,8 +1,8 @@
-package com.company.Gioco.Pezzi;
+package com.Gioco.Pezzi;
 
-import com.company.Gioco.BloccoPieno;
-import com.company.Gioco.Griglia;
-import com.company.Gioco.Pezzo;
+import com.Gioco.BloccoPieno;
+import com.Gioco.Griglia;
+import com.Gioco.Pezzo;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 
@@ -11,17 +11,17 @@ public class PezzoS extends Pezzo {
     public TextColor colore = TextColor.ANSI.BLUE_BRIGHT;
 
     public PezzoS(TextGraphics schermo, Griglia campo) {
-        super(campo,new BloccoPieno(schermo, 4, 0, TextColor.ANSI.BLUE_BRIGHT),
+        super(campo, new BloccoPieno(schermo, 4, 0, TextColor.ANSI.BLUE_BRIGHT),
                 new BloccoPieno(schermo, 5, 0, TextColor.ANSI.BLUE_BRIGHT),
                 new BloccoPieno(schermo, 3, 1, TextColor.ANSI.BLUE_BRIGHT),
                 new BloccoPieno(schermo, 4, 1, TextColor.ANSI.BLUE_BRIGHT));
         tipoPezzo = 4;
-        rotazione = 0;
+        rotazione = 1;
 
         maxRotazioni = 1;
 
-        spostamentoVerticale= new int[][]{{0, 2, 0, 0}, {0, -2, 0, 0}};
-        spostamentoOrizzontale = new int[][]{{-1, -1, 0, 0}, {1, 1, 0, 0}};
+        spostamentoVerticale= new int[][]{{0, 1, 0, 1}, {0, -1, 0, -1}};
+        spostamentoOrizzontale = new int[][]{{0, 1, -2, -1}, {0, -1, 2, 1}};
 
     }
 
@@ -29,16 +29,20 @@ public class PezzoS extends Pezzo {
     public void ruota(Griglia campo, int rotazione, int verso){
 
        for(int i=3; i>=0; i--){
-       pezzo[i].rimuovi(campo, pezzo[i].getColonna(), pezzo[i].getRiga());
-        /* }
-        for(int i=3;i>=0;i--){*/
-       pezzo[i].muovi(campo, pezzo[i].getColonna(), pezzo[i].getRiga(), spostamentoOrizzontale[rotazione][i], spostamentoVerticale[rotazione][i],  colore);
-       pezzo[i].colonnaGriglia = pezzo[i].getColonna() + spostamentoOrizzontale[rotazione][i];
-       pezzo[i].rigaGriglia = pezzo[i].getRiga() + spostamentoVerticale[rotazione][i];
+            pezzo[i].rimuovi(campo, pezzo[i].getColonna(), pezzo[i].getRiga());
        }
-       if(rotazione!=maxRotazioni)
-         this.rotazione++;
-       else
-         this.rotazione=0;
+        for(int i=3;i>=0;i--){
+           pezzo[i].muovi(campo, pezzo[i].getColonna(), pezzo[i].getRiga(), spostamentoOrizzontale[rotazione][i]*verso, spostamentoVerticale[rotazione][i]*verso,  colore);
+           pezzo[i].colonnaGriglia = pezzo[i].getColonna() + spostamentoOrizzontale[rotazione][i]*verso;
+           pezzo[i].rigaGriglia = pezzo[i].getRiga() + spostamentoVerticale[rotazione][i]*verso;
+       }
+        if(verso!=-1) {
+            if (rotazione != maxRotazioni) {
+                this.rotazione++;
+            } else {
+                this.rotazione = 0;
+            }
+        } else
+            this.rotazione=rotazione;
     }
 }
