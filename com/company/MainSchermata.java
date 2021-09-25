acf25de13fa0dd6fe7096c459a0fd52d44a004fc
Luca Palmieri <luca.palmieri12@studio.unibo.it>
2021-09-25 11:14:28 +0000

    Update regole di gioco

--- com/company/MainSchermata.java
+++ com/company/MainSchermata.java
@@ -321,22 +321,24 @@ public class MainSchermata {
                 //svuoto lo schermo
                 panel.removeAllComponents();
                 panel.setFillColorOverride(BLACK);
-               Label regolamento = new Label("Benvenuto in questa nuova versione multiplayer di Tetris™!\n\nRegolamento di gioco:" +
-                        "\n -Una partita è formata da un minimo di 2 a un massimo di \n4 giocatori connessi ad un server;" +
-                        "\n\n -Ogni giocatore deve ruotare i pezzi che cadono in modo \ntale da riempire una riga orizzontale di blocchi senza interruzioni;" +
-                        "\n\n -Quando si riempie una riga orizzontale questa diventa una \n'riga spazzatura' che viene inviata all'avversario selezionato;" +
-                        "\n\n -Game Over: i blocchi si accumulano fino alla sommità \ndell'area di gioco;" +
-                        "\n\n -Vittoria: tutti gli avversari hanno subito Game Over \nmentre tu sei rimasto ancora in gioco;" +
+   Label regolamento = new Label("Benvenuto in questa nuova versione multiplayer di Tetris™!\n\nRegolamento di gioco:" +
+                        "\n\n -Una partita è formata da un minimo di 2 a un massimo di \n  4 giocatori connessi ad un server;" +
+                        "\n\n -Ogni giocatore deve ruotare i pezzi che cadono in modo \n  tale da riempire una riga orizzontale di blocchi senza interruzioni;" +
+                        "\n\n -Quando si riempie una riga orizzontale questa diventa una \n  'riga spazzatura' che viene inviata all'avversario selezionato;" +
+                        "\n\n -Game Over: i blocchi si accumulano fino alla sommità \n  dell'area di gioco;" +
+                        "\n\n -Vittoria: tutti gli avversari hanno subito Game Over \n  mentre tu sei rimasto ancora in gioco;" +
                         "\n\n\n Comandi utili:" +
                         "\n\n Server:" +
                         "\n /start: fai partire il gioco se ci sono almeno 2 giocatori" +
                         "\n /quit o close: interrompi la partita ed esci" +
                         "\n /pause: metti in pausa la partita" +
                         "\n /resume: riprendi una partita in pausa" +
-                        "\n /restart: ricomincia una partita dall'inizio " +
+                        "\n /startagain: ricomincia una partita dall'inizio " +
+                        "\n /restart: termina la partita e disconnette i client" +
                         "\n\n Giocatore:" +
                         "\n Frecce direzionali ← ↓ →: movimento del pezzo" +
-                        "\n x: ruota il pezzo in senso orario" +
+                        "\n s: ruota il pezzo in senso antiorario" +
+                        "\n d: ruota il pezzo in senso orario" +
                         "\n Barra spaziatrice: fai cadere il pezzo istantaneamente" +
                         "\n Numeri 1 2 3: seleziona l'avversario a cui inviare le righe spazzatura.").setBackgroundColor(BLACK).setForegroundColor(coloreLabel);
                
